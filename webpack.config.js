12831ca2bfdced87ec8d00a7b3292bf3f796db4a
Ken Wheeler <ken_wheeler@me.com>
2015-07-01 09:07:13 -0400

    Babel decorator support

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 var getConfig = require('hjs-webpack')
 var config = require('./presentation/config');
 
-module.exports = getConfig({
+var webpackConfig = getConfig({
   in: './index.jsx',
   out: 'dist',
   clearBeforeBuild: true,
   html: config.html
 });
+
+webpackConfig.module.loaders[0] = {
+  test: /(\.js$)|(\.jsx$)/,
+  exclude: /node_modules/,
+  loaders: [
+    'babel-loader?stage=1'
+  ]
+}
+
+module.exports = webpackConfig;
