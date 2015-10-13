8bce3172f804a25c916ce53b57533dbead2210ca
Jason Trill <jason@jasontrill.com>
2015-10-13 08:53:37 -0700

    Expose mdast config, better source/children check

--- src/markdown.jsx
+++ src/markdown.jsx
@@ -1,6 +1,7 @@
 import React from "react/addons";
 import mdast from "mdast";
 import mdastReact from "mdast-react";
+import isUndefined from "lodash/lang/isundefined";
 
 import CodePane from "./code-pane";
 import Heading from "./heading";
@@ -23,34 +24,10 @@ function spectacleComponent(component, boundProps = {}) {
   });
 }
 
-// Would like to expose this config to the user
-const mdastConfig = {
-  commonmark: true,
-  mdastReactComponents: {
-    a: Link,
-    code: CodePane,
-    del: spectacleComponent(S, {type: "strikethrough"}),
-    em: spectacleComponent(S, {type: "italic"}),
-    h1: spectacleComponent(Heading, {size: 1}),
-    h2: spectacleComponent(Heading, {size: 2}),
-    h3: spectacleComponent(Heading, {size: 3}),
-    h4: spectacleComponent(Heading, {size: 4}),
-    h5: spectacleComponent(Heading, {size: 5}),
-    h6: spectacleComponent(Heading, {size: 6}),
-    img: Image,
-    li: ListItem,
-    p: Text,
-    strong: spectacleComponent(S, {type: "bold"}),
-    ul: List
-  }
-};
-
-
 export default class Markdown extends React.Component {
   render() {
-    const content = typeof this.props.source !== "undefined" ?
-      this.props.source :
-      this.props.children;
+    const { source, children, mdastConfig } = this.props;
+    const content = (isUndefined(source) || source === "") ? children : source;
 
     return (
       <div>
@@ -64,3 +41,27 @@ Markdown.propTypes = {
   children: React.PropTypes.node,
   source: React.PropTypes.string
 }
+
+Markdown.defaultProps = {
+  source: "",
+  mdastConfig: {
+    commonmark: true,
+    mdastReactComponents: {
+      a: Link,
+      code: CodePane,
+      del: spectacleComponent(S, {type: "strikethrough"}),
+      em: spectacleComponent(S, {type: "italic"}),
+      h1: spectacleComponent(Heading, {size: 1}),
+      h2: spectacleComponent(Heading, {size: 2}),
+      h3: spectacleComponent(Heading, {size: 3}),
+      h4: spectacleComponent(Heading, {size: 4}),
+      h5: spectacleComponent(Heading, {size: 5}),
+      h6: spectacleComponent(Heading, {size: 6}),
+      img: Image,
+      li: ListItem,
+      p: Text,
+      strong: spectacleComponent(S, {type: "bold"}),
+      ul: List
+    }
+  }
+};
