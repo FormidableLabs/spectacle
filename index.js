8aab00be14c972539b4e73aead8118d80ebc1511
Mateusz Zatorski <mateuszzatorski@gmail.com>
2015-11-23 00:18:15 +0000

    fix build script, add prod webpack config

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import context from "./src/utils/context";
 
 import { Router, Route } from "react-router";
 import createBrowserHistory from "history/lib/createBrowserHistory";
+import createHashHistory from "history/lib/createHashHistory";
 
 import Alt from "alt";
 import Flux from "./src/flux/alt";
@@ -14,6 +15,10 @@ import config from "./presentation/config";
 require("normalize.css");
 require("./themes/default/index.css");
 require("highlight.js/styles/monokai_sublime.css");
+
+const history = process.env.NODE_ENV === "production" ?
+  createHashHistory() :
+  createBrowserHistory();
 
 const flux = new Flux();
 Alt.debug("flux", flux);
@@ -28,7 +33,7 @@ Presentation.contextTypes = {
 const PresentationContext = context(Presentation, {styles: config.theme, print: config.print, flux});
 
 render(
-  <Router history={createBrowserHistory()}>
+  <Router history={history}>
     <Route path="/" component={PresentationContext} />
     <Route path="/:slide" component={PresentationContext} />
   </Router>
---
+++ webpack.config.production.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  entry: "./index",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        "NODE_ENV": JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel"
+    }, {
+      test: /\.css$/,
+      loader: "style-loader!css-loader"
+    }, {
+      test: /\.(png|jpg)$/,
+      loader: "url-loader?limit=8192"
+    }, {
+      test: /\.svg$/,
+      loader: "url?limit=10000&mimetype=image/svg+xml"
+    }]
+  }
+};
