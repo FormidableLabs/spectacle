69193944c0ff571e3de6ef4af5321045c4f45865
Mateusz Zatorski <mateuszzatorski@gmail.com>
2015-12-22 21:38:52 +0100

    change single for double quotes

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,17 @@
-/* eslint-disable */
-
-var path = require('path');
-var webpack = require('webpack');
+var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: "source-map",
   entry: [
-    'webpack-hot-middleware/client',
-    'babel-polyfill',
-    './index'
+    "webpack-hot-middleware/client",
+    "babel-polyfill",
+    "./index"
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/dist/'
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist/"
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
@@ -22,18 +20,18 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.js$/,
-      loader: 'babel',
+      loader: "babel",
       query: {
         plugins: [
           [
-            'react-transform', {
+            "react-transform", {
               transforms: [{
-                transform: 'react-transform-hmr',
-                imports: ['react'],
-                locals: ['module']
+                transform: "react-transform-hmr",
+                imports: ["react"],
+                locals: ["module"]
               }, {
-                transform: 'react-transform-catch-errors',
-                imports: ['react', 'redbox-react']
+                transform: "react-transform-catch-errors",
+                imports: ["react", "redbox-react"]
               }]
             }
           ]
@@ -43,20 +41,20 @@ module.exports = {
       include: __dirname
     }, {
       test: /\.css$/,
-      loaders: ['style', 'raw'],
+      loaders: ["style", "raw"],
       include: __dirname
     }, {
       test: /\.svg$/,
-      loader: 'url?limit=10000&mimetype=image/svg+xml',
-      include: path.join(__dirname, 'assets')
+      loader: "url?limit=10000&mimetype=image/svg+xml",
+      include: path.join(__dirname, "assets")
     }, {
       test: /\.png$/,
-      loader: 'url-loader?mimetype=image/png',
-      include: path.join(__dirname, 'assets')
+      loader: "url-loader?mimetype=image/png",
+      include: path.join(__dirname, "assets")
     }, {
       test: /\.jpg$/,
-      loader: 'url-loader?mimetype=image/jpg',
-      include: path.join(__dirname, 'assets')
+      loader: "url-loader?mimetype=image/jpg",
+      include: path.join(__dirname, "assets")
     }]
   }
 };
