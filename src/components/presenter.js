a7e791d7e0ffa77eeeae7e56bcb68963e86e937e
Dustan Kasten <dustan.kasten@gmail.com>
2016-01-13 21:55:54 -0500

    dub dub quotes

--- src/components/presenter.js
+++ src/components/presenter.js
@@ -80,7 +80,7 @@ export default class Presenter extends Component {
   _renderNotes() {
     const child = this.props.slides[this.props.slide];
     if (!child.props.notes) { return false; }
-    if (typeof child.props.notes === 'string') {
+    if (typeof child.props.notes === "string") {
       return <div dangerouslySetInnerHTML={{__html: child.props.notes}} />;  
     }
     return <div>{child.props.notes}</div>;
