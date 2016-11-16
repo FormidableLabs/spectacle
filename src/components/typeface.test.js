42bbe9f25cf78e0628b32f1328c0d0d6c36d32c0
Carlos Paelinck <carlos.paelinck@formidable.com>
2016-11-16 11:37:12 -0800

    Add tests for Typeface system and Google font tags

---
+++ src/components/typeface.test.js
@@ -0,0 +1,51 @@
+import React, { Component, PropTypes } from "react";
+import { mount } from "enzyme";
+import { mountToJson } from "enzyme-to-json";
+import Typeface from "./typeface";
+
+class MockComponent extends Component {
+  static propTypes = {
+    children: PropTypes.node
+  };
+
+  static contextTypes = {
+    typeface: PropTypes.object
+  };
+
+  render() {
+    return (
+      <div
+        style={this.context.typeface}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+describe("<Typeface />", () => {
+  test("should render the children when using a system font.", () => {
+    const wrapper = mount(
+      <Typeface
+        font="SF UI Text"
+        weight={400}
+      >
+        <MockComponent>Hello!</MockComponent>
+      </Typeface>
+    );
+    expect(mountToJson(wrapper)).toMatchSnapshot();
+  });
+
+  test("should render the children when using a Google font.", () => {
+    const wrapper = mount(
+      <Typeface
+        googleFont="Roboto Slab"
+        weight={700}
+        italic
+      >
+        <MockComponent>Hello!</MockComponent>
+      </Typeface>
+    );
+    expect(mountToJson(wrapper)).toMatchSnapshot();
+  });
+});
