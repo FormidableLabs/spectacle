807a2b741be209c73c8ee78473c81390b219f8df
Carlos Paelinck <carlos.paelinck@formidable.com>
2017-01-18 16:29:09 -0600

    Upgrade to Webpack 2.2

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin()
   ],
   module: {
     loaders: [{
