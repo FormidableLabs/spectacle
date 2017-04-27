b8df037cea9824ff5afd0cbaef6f26353494a2c8
Brian Beck <exogen@gmail.com>
2017-04-27 15:22:29 -0700

    Change devtool to speed up dev build performance

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 var webpack = require("webpack");
 
 module.exports = {
-  devtool: "source-map",
+  devtool: "cheap-module-source-map",
   entry: [
     "webpack-hot-middleware/client",
     "babel-polyfill",
