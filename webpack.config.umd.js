f88213fc1f265bb217f1b3f629753d91f93d0e16
Ken Wheeler <thekenwheeler@gmail.com>
2017-08-07 17:10:56 -0400

    lint

--- webpack.config.umd.js
+++ webpack.config.umd.js
@@ -14,27 +14,27 @@ module.exports = {
   },
   externals: [
     {
-      "react": {
-        root: "React",
-        commonjs2: "react",
-        commonjs: "react",
-        amd: "react"
+      'react': {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
       }
     },
     {
-      "react-dom": {
-        root: "ReactDOM",
-        commonjs2: "react-dom",
-        commonjs: "react-dom",
-        amd: "react-dom"
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom'
       }
     },
     {
-      "prop-types": {
-        root: "PropTypes",
-        commonjs2: "prop-types",
-        commonjs: "prop-types",
-        amd: "prop-types"
+      'prop-types': {
+        root: 'PropTypes',
+        commonjs2: 'prop-types',
+        commonjs: 'prop-types',
+        amd: 'prop-types'
       }
     }
   ],
