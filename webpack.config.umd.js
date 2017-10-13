f4895684547d701e1a475c195463809644747b8c
zurfyx <zurfyx@gmail.com>
2017-10-13 12:36:21 +0200

    Added .example loader to webpack config files

--- webpack.config.umd.js
+++ webpack.config.umd.js
@@ -58,6 +58,9 @@ module.exports = {
     }, {
       test: /\.svg$/,
       loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+    }, {
+      test: /\.example$/,
+      loader: 'raw-loader'
     }]
   }
 };
