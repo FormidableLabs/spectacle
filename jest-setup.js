6b13ee4455ea570527fb3abd61a2f409e4214b77
imranolas <imran.sulemanji@gmail.com>
2017-10-13 20:55:03 +0100

    Handle warning: Add raf polyfill

--- jest-setup.js
+++ jest-setup.js
@@ -2,3 +2,6 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
+document.requestAnimationFrame = function(callback) {
+  setTimeout(callback, 0);
+};
