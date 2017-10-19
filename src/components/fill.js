2fc37d275e01a3c76ccac5d5892462a303add56b
Carlos Paelinck <carlos.paelinck@formidable.com>
2017-10-19 15:24:56 -0500

    Start transition to Emotion. Fix Prism.

--- src/components/fill.js
+++ src/components/fill.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled from 'react-emotion';
 
 export const Fill = styled.div`
   flex: 1;
--- src/components/presenter-components.js
+++ src/components/presenter-components.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'react-emotion';
 
 export const ContentContainer = styled.div`
   display: flex;
