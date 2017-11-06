572f27b92cb82d163dffb33e927c571588c90d20
Phil Plückthun <phil@plckthn.me>
2017-11-06 16:10:58 +0000

    Move Pacman keyframes from index.css into emotion

--- src/components/progress.js
+++ src/components/progress.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'react-emotion';
+import styled, { keyframes } from 'react-emotion';
 
 const Pacman = {
   Base: styled.div(({ styles, position }) => [ styles, position ]),
   Body: styled.div(props => props.styles)
 };
+
+// NOTE: rotateZ is 0.1 to generate two different animation names (emotion deduplication)
+const pacmanTopFrames = keyframes`
+  0% { transform: rotateZ(0.1deg) }
+  100% { transform: rotateZ(-30deg) }
+`;
+
+// NOTE: rotateZ is 0.1 to generate two different animation names (emotion deduplication)
+const pacmanBottomFrames = keyframes`
+  0% { transform: rotateZ(0.1deg) }
+  100% { transform: rotateZ(30deg) }
+`;
+
+const pacmanTopFramesBis = keyframes`
+  0% { transform: rotateZ(0deg) }
+  100% { transform: rotateZ(-30deg) }
+`;
+
+const pacmanBottomFramesBis = keyframes`
+  0% { transform: rotateZ(0deg) }
+  100% { transform: rotateZ(30deg) }
+`;
 
 const Point = styled.div(({ styles, position }) => [ styles, position ]);
 const Bar = styled.div(({ styles, width }) => [ styles, width ]);
@@ -19,7 +41,15 @@ export default class Progress extends Component {
   }
 
   getPacmanStyle(side) {
-    const animationName = `pacman${side}Frames${(this.props.currentSlideIndex % 2 ? '' : 'Bis')}`;
+    const isBis = this.props.currentSlideIndex % 2 !== 0;
+    let animationName;
+
+    if (side === 'top') {
+      animationName = isBis ? pacmanTopFramesBis : pacmanTopFrames;
+    } else {
+      animationName = isBis ? pacmanBottomFramesBis : pacmanBottomFrames;
+    }
+
     return {
       animation: `${animationName} 0.12s linear 10 alternate both`
     };
@@ -56,8 +86,8 @@ export default class Progress extends Component {
               styles={style.pacman}
               position={this.getPointPosition(currentSlideIndex)}
             >
-              <Pacman.Body styles={[style.pacmanTop, this.getPacmanStyle('Top')]} />
-              <Pacman.Body styles={[style.pacmanBottom, this.getPacmanStyle('Bottom')]} />
+              <Pacman.Body styles={[style.pacmanTop, this.getPacmanStyle('top')]} />
+              <Pacman.Body styles={[style.pacmanBottom, this.getPacmanStyle('bottom')]} />
             </Pacman.Base>
             {items.map((item, i) => {
               return (
