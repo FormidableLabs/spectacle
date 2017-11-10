2bc6ad8a0ee549751dfff72b17792fe7ded02f00
Phil Plückthun <phil@plckthn.me>
2017-11-10 14:38:05 +0000

    Ignore non-DOM Editor props
    - Filter style props that are only used for Emotion in the Editor
      components
    - Edit ESLint no-unused-vars rule to ignore /^_+$/ variables

--- src/components/code-pane.js
+++ src/components/code-pane.js
@@ -7,7 +7,9 @@ import '../utils/prism-import';
 import { Editor } from 'react-live';
 
 const StyledWrapper = styled.div(props => props.styles);
-const StyledEditor = styled(Editor)`
+const StyledEditor = styled(({ syntaxStyles: _, prismTheme: __, ...rest }) => {
+  return <Editor {...rest} />;
+})`
   && {
     ${props => props.syntaxStyles}
   }
