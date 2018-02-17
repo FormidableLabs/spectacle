0d80be21bedd91bdd641ce373d956b6cb18632e7
Kylie Stewart <kylie.stewart@me.com>
2018-02-16 17:00:36 -0700

    refactored and renamed

--- bin/files.js
+++ bin/files.js
@@ -1,73 +1,28 @@
-const fs = require('fs');
-const path = require('path');
-const touch = require('touch');
-const { mkDirByPathSync } = require('./utils');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
 
-// TODO_REFACTOR 
+// remove /presentation-blog/ folder if it already exists
+exports.removeDir = function removeDir(path) {
+  return new Promise((resolve, reject) => {
+    rimraf(path, {}, (err) => {
+      if (err) {
+        reject(err);
+      }
 
-const slides = [{
-  deckTitle: "Spectacle is Great",
-  slideTitle: "simple cool presentations",
-  notes: "<li><ul>Things</ul><ul>Things</ul></li>",
-  date: "01-16-2018"
-},{
-  deckTitle: "Spectacle is Great",
-  slideTitle: "custom presentations",
-  notes: "<li><ul>Other things</ul><ul>Other things</ul></li>",
-  date: "01-31-2018"
-},{
-  deckTitle: "Spectacle is Great",
-  slideTitle: "special styles in your presentations",
-  notes: "<li><ul>Other things</ul><ul>Other things</ul></li>",
-  date: "02-13-2018"
-}];
-
-const src = process.cwd(); 
-const name = path.basename(src);
-const blogPath = path.join(`${name}-blog/pages`);
-
-function createOrReadBase() { 
-  return new Promise((fulfill, reject) => {
-    mkDirByPathSync(blogPath);
-    fulfill();
-})};
-
-function getPath(slideTitle, date) {
-  return slideTitle.toLowerCase().split(' ', 2).join('-');
-}
-
-function getFolderPath(gatsbyPath, date) {
-  return `${date}-${gatsbyPath}`; 
-}
-
-exports.createPost = function createPost(slide) {
-  const gatsbyPath = getPath(slide.slideTitle);
-  const gatsbyFolder = getFolderPath(gatsbyPath, slide.date);
-  
-  return createOrReadBase().then(() => {
-    console.log(`A folder has been created at ${blogPath}/${gatsbyFolder}`);
-    mkDirByPathSync(`${blogPath}/${gatsbyFolder}`);
-    return touch(`${blogPath}/${gatsbyFolder}/index.md`);
-  })
-}
-
-exports.writeGatsbyStream = function writeGatsbyStream(slide) {
-  const { slideTitle, date, notes } = slide;
-  const path = getPath(slideTitle);
-  const folder = getFolderPath(path, date);
-  const stream = fs.createWriteStream(`${blogPath}/${folder}/index.md`);
-
-  stream.once('open', (fd) => {
-    stream.write(`----\n`);
-    stream.write(`path: ${path}\n`);
-    stream.write(`date: ${date}\n`);
-    stream.write(`title: ${slideTitle}\n`);
-    stream.write(`----\n`);
-    stream.write(`${notes}\n`);
-    stream.end();
+      resolve();
+    });
   });
+}
 
-  stream.on('finish', () => {  
-    console.log('wrote all data to file!');
+// create /presentation-blog/ folder 
+exports.createDir = function createDir(path) {
+  return new Promise((resolve, reject) => {
+    mkdirp(path, (err) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve();
+    });
   });
 }
