f15e5c88f16b8c7522911e54e6dd8126bf729fab
Alex Lande <alexlande@gmail.com>
2018-03-12 11:16:35 -0700

    Add aria-label to fullscreen button (#471)

--- src/components/fullscreen-button.js
+++ src/components/fullscreen-button.js
@@ -18,7 +18,7 @@ const Button = styled.button`
 `;
 
 const FullscreenButton = props => (
-  <Button {...props}>
+  <Button aria-label="Toggle full screen" {...props}>
     <svg viewBox="0 0 24 24">
       <path d="M0 0h24v24H0z" fill="none" />
       <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z" fill="currentColor" />
