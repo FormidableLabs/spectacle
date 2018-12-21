d9b29154e2759f2c1f212dbe8ac9c7371b3b892e
Tegan Churchill <churchill.tegan@gmail.com>
2018-12-21 13:03:04 -0800

    Remove warnings from test suite

--- src/components/heading.js
+++ src/components/heading.js
@@ -85,9 +85,10 @@ export default class Heading extends Component {
       const text = this.textRef;
       const container = this.containerRef;
       text.style.display = 'inline-block';
-      const scale = container.offsetWidth / text.offsetWidth;
+      const scale = container.offsetWidth / text.offsetWidth || 0;
       const height = text.offsetHeight * scale || 0;
       text.style.display = 'block';
+
       this.setState({
         scale,
         height
--- src/components/time.js
+++ src/components/time.js
@@ -1,27 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Clock from './clock';
 import Timer from './timer';
 import { TimeContainer } from './time-components';
 
-export default class Time extends Component {
-  constructor() {
-    super(...arguments);
-    this.state = { timer: false };
-  }
-  _renderClock() {
-    if (this.state.timer) {
-      return <Timer />;
-    } else {
-      return <Clock />;
-    }
-  }
-  render() {
-    return (
-      <TimeContainer>{this.props.timer ? <Timer /> : <Clock />}</TimeContainer>
-    );
-  }
+export default function Time({ timer }) {
+  return <TimeContainer>{timer ? <Timer /> : <Clock />}</TimeContainer>;
 }
+
 Time.propTypes = {
   timer: PropTypes.bool
 };
