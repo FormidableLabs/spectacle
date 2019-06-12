08e2ec46786873005de16df400f4927cfff5f387
Dominic Coelho <dominic.coelho@formidable.com>
2019-06-12 15:59:35 +0100

    1. Add documentation for index.js and test.mdx.

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import MDXDocument from './test.mdx';
 
-// const Zip = () => <div>boop</div>;
+/**
+ * Experiment to test MDX -> JSX transpilation through babel.
+ *
+ * Outputs MDXDocument, changing MDXDocument will cause webpack
+ * to hot-reload with new contents.
+ */
+
 render(<MDXDocument />, document.getElementById('root'));
