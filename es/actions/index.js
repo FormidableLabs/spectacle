33772bb54f9ca2997a7caa10fab8c02c430ed599
Dominic Coelho <dominic.coelho@formidable.com>
2019-06-12 15:21:01 +0100

    remove files unintentionally carried into branch

--- es/actions/index.js
+++
@@ -1,5 +0,0 @@
-import { createAction } from 'redux-actions';
-export var addFragment = createAction('ADD_FRAGMENT');
-export var updateFragment = createAction('UPDATE_FRAGMENT');
-export var updateRoute = createAction('UPDATE_ROUTE');
-export var setGlobalStyle = createAction('SET_GLOBAL_STYLE');
