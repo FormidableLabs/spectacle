4337697742de2ee9e7f66d04ee5d5dc1013a3190
kiraarghy <karaestubbs@gmail.com>
2019-06-20 16:28:05 +0100

    rewrite/add-deck
    * Implemented springs for SlideElementWrappers
    * SlideElementWrapper can accept custom springs now!

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -2,7 +2,7 @@ import React from 'react';
 import useSlide, { SlideContext } from '../hooks/useSlide';
 import { DeckContext } from '../hooks/useDeck';
 import isComponentType from '../utils/isComponentType.js';
-import SlideElementWrapper from './SlideElementWrapper'
+import SlideElementWrapper from './SlideElementWrapper';
 
 /**
  *
@@ -15,12 +15,18 @@ const Slide = props => {
   const { children, slideNum } = props;
 
   const numberOfSlideElements = Array.isArray(children)
-    ? children.filter(x => isComponentType(x, 'SlideElementWrapper')).length
+    ? children.filter(x => isComponentType(x, 'SlideElementWrapper')).length + 1
     : 0;
   const isActive = slideNum === state.currentSlide;
 
   return (
-    <div style={{ backgroundColor: 'lavender', border: '2px solid plum' }}>
+    <div
+      style={{
+        backgroundColor: 'lavender',
+        border: '2px solid plum',
+        overflow: 'hidden'
+      }}
+    >
       <p>{slideNum}</p>
       <SlideContext.Provider
         value={useSlide(initialState, isActive, numberOfSlideElements)}
--- src/components/SlideElementWrapper.js
+++ src/components/SlideElementWrapper.js
@@ -1,22 +1,38 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring';
 
 import useSlide, { SlideContext } from '../hooks/useSlide';
+
+/**
+ *
+ *
+ */
 
 const SlideElementWrapper = props => {
   const { elementNum, transitionEffect, children } = props;
   const [state] = React.useContext(SlideContext);
 
-  return (
-    <div
-      style={
-        state && elementNum === state.currentSlideElement
-          ? { border: '2px solid orange' }
-          : { display: 'none' }
-      }
-    >
-      {children}
-    </div>
+  const [styleProps, set] = useSpring(() =>
+    transitionEffect
+      ? console.log(transitionEffect.from) || transitionEffect.from
+      : { opacity: 0 }
   );
+
+  React.useEffect(() => {
+    if (state && elementNum <= state.currentSlideElement) {
+      if (transitionEffect) {
+        console.log(transitionEffect.to);
+        set({
+          to: transitionEffect.to
+        });
+      }
+      set({
+        opacity: 1
+      });
+    }
+  }, [state]);
+
+  return <animated.div style={styleProps}>{children}</animated.div>;
 };
 
 export default SlideElementWrapper;
