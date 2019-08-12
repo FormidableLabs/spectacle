a9d7c2a947e58820c95057c93dc44d75ef89c734
Jon Reynolds <jon.reynolds456@gmail.com>
2019-08-12 12:44:57 -0600

    pass linting & prettier

--- examples/JS/TestJS.js
+++ examples/JS/TestJS.js
@@ -12,12 +12,12 @@ const TestJs = () => (
     <Slide>
       <p> Slide 3! </p>
       <SlideElementWrapper elementNum={0}>
-        <div>Hey, just one "animated" slide element here</div>
+        <div>{`Hey, just one "animated" slide element here`}</div>
       </SlideElementWrapper>
     </Slide>
     <Slide>
-      <p>I'm a static slide element that should always show</p>
-      <p>This means that we don't need a SlideElementWrapper</p>
+      <p>{`I'm a static slide element that should always show`}</p>
+      <p>{`This means that we don't need a SlideElementWrapper`}</p>
       <SlideElementWrapper elementNum={0}>
         <p slide-element="true"> ZERO Slide 4 x 3! </p>
       </SlideElementWrapper>
@@ -27,7 +27,7 @@ const TestJs = () => (
       <SlideElementWrapper elementNum={2}>
         <p slide-element="true"> TWO Slide 4 x 3! </p>
       </SlideElementWrapper>
-      <p>I'm also a static non-animated "slide element"!</p>
+      <p>{`I'm also a static non-animated "slide element"!`}</p>
     </Slide>
     <div>HEY PHIL. YOU DOUBTED US???</div>
   </Deck>
