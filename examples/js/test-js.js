ed98b96f3007b06d13d64874158e7292511df5ba
Narin Luangrath <narin.luangrath@gmail.com>
2019-08-30 17:26:37 -0700

    Refactor use-presentation and add example

--- examples/js/test-js.js
+++ examples/js/test-js.js
@@ -3,6 +3,7 @@ import Deck from '../../src/components/deck.js';
 import Slide from '../../src/components/slide.js';
 import SlideElementWrapper from '../../src/components/slide-element-wrapper';
 import CodePane from '../../src/components/code-pane';
+import usePresentation from '../../src/hooks/use-presentation';
 
 const reactJSCodeBlock = `
 export default function CodePane(props) {
@@ -45,38 +46,75 @@ int main()
 }
 `;
 
-const TestJs = () => (
-  <Deck>
-    <Slide slideNum={1}>
-      <CodePane language="jsx">{reactJSCodeBlock}</CodePane>
-    </Slide>
-    <Slide slideNum={2}>
-      <CodePane googleFont="Space Mono" fontSize={20} language="cpp">
-        {cppCodeBlock}
-      </CodePane>
-    </Slide>
-    <Slide slideNum={3}>
-      <p> Slide 3! </p>
-      <SlideElementWrapper elementNum={1}>
-        <div>{`Hey, just one "animated" slide element here`}</div>
-      </SlideElementWrapper>
-    </Slide>
-    <Slide slideNum={4}>
-      <p>{`I'm a static slide element that should always show`}</p>
-      <p>{`This means that we don't need a SlideElementWrapper`}</p>
-      <SlideElementWrapper elementNum={1}>
-        <p> ZERO Slide 4 x 3! </p>
-      </SlideElementWrapper>
-      <SlideElementWrapper elementNum={2}>
-        <p> ONE Slide 4 x 3! </p>
-      </SlideElementWrapper>
-      <SlideElementWrapper elementNum={3}>
-        <p> TWO Slide 4 x 3! </p>
-      </SlideElementWrapper>
-      <p>{`I'm also a static non-animated "slide element"!`}</p>
-    </Slide>
-    <div>HEY PHIL. YOU DOUBTED US???</div>
-  </Deck>
-);
+const TestJs = () => {
+
+  const {     
+    startConnection,
+    terminateConnection,
+    sendMessage,
+    errors,
+    addMessageHandler,
+    isReceiver,
+    isController
+  } = usePresentation();
+
+  const [messages, setMessages] = React.useState([]);
+
+  // The dependencies are [isReceiver] as opposed to [] because
+  // The user might exit out of the presentation and start it up again, so
+  // we'll need to re-add the messageHandlers.
+  React.useEffect(() => {
+    addMessageHandler(msg => setMessages(prev => ([ ...prev, msg])));
+  }, [isReceiver])
+
+  const sendHello = () => sendMessage("Hello");
+
+  return (
+    <React.Fragment>
+      {!isController && !isReceiver && <button onClick={startConnection}>Start Connection</button>}
+      {isController && !isReceiver && <button onClick={terminateConnection}>Terminate Connection</button>}
+      {isController && !isReceiver && <button onClick={sendHello}>Send "Hello"</button>}
+      {isReceiver && (
+        <div>
+          <h1>Messages</h1>
+          <ul>
+            {messages.map(message => <li key={message}>{message}</li>)}
+          </ul>
+        </div>
+      )}
+      <Deck>
+        <Slide slideNum={1}>
+          <CodePane language="jsx">{reactJSCodeBlock}</CodePane>
+        </Slide>
+        <Slide slideNum={2}>
+          <CodePane googleFont="Space Mono" fontSize={20} language="cpp">
+            {cppCodeBlock}
+          </CodePane>
+        </Slide>
+        <Slide slideNum={3}>
+          <p> Slide 3! </p>
+          <SlideElementWrapper elementNum={1}>
+            <div>{`Hey, just one "animated" slide element here`}</div>
+          </SlideElementWrapper>
+        </Slide>
+        <Slide slideNum={4}>
+          <p>{`I'm a static slide element that should always show`}</p>
+          <p>{`This means that we don't need a SlideElementWrapper`}</p>
+          <SlideElementWrapper elementNum={1}>
+            <p> ZERO Slide 4 x 3! </p>
+          </SlideElementWrapper>
+          <SlideElementWrapper elementNum={2}>
+            <p> ONE Slide 4 x 3! </p>
+          </SlideElementWrapper>
+          <SlideElementWrapper elementNum={3}>
+            <p> TWO Slide 4 x 3! </p>
+          </SlideElementWrapper>
+          <p>{`I'm also a static non-animated "slide element"!`}</p>
+        </Slide>
+        <div>HEY PHIL. YOU DOUBTED US???</div>
+      </Deck>
+    </React.Fragment>
+  )
+};
 
 export default TestJs;
--- src/hooks/use-presentation.js
+++ src/hooks/use-presentation.js
@@ -1,41 +1,25 @@
 import { useEffect, useState, useCallback, useMemo, useRef } from 'react';
 
+function getReceiver() {
+  return (
+    window.navigator && 
+    window.navigator.presentation && 
+    window.navigator.presentation.receiver
+  );
+}
+
 function usePresentation() {
   const [connection, setConnection] = useState(null);
-  const [isReceiver, setIsReceiver] = useState(false);
-
-  // moved to using ref as we don't want messagehandlers to change after invocation
-  const messageHandlers = useRef({});
-  const requestRef = useRef(null);
   const [errors, setErrors] = useState([]);
 
-  // add message handler handles adding and removing handlers
-  const addMessageHandler = useCallback((handler, key) => {
-    const keyHandlers = (messageHandlers.current[key] =
-      messageHandlers.current[key] || []);
-    keyHandlers.push(handler);
-    return () => {
-      // if it exists then remove it from the messageHandler in cleanup
-      const keyHandlers = messageHandlers.current[key];
-      if (keyHandlers) {
-        keyHandlers.splice(keyHandlers.indexOf(handler), 1);
-      }
-    };
-  }, []);
+  const requestRef = useRef(null);
 
   // Open to suggestions for better error handling
   const addError = e => setErrors(es => [...es, e]);
 
-  const terminateConnection = useCallback(() => {
-    if (connection) {
-      connection.terminate();
-      setConnection(null);
-    }
-  }, [connection]);
-
   // Create a presentation request and store it as a ref
   useEffect(() => {
-    if (PresentationRequest) {
+    if (window.PresentationRequest) {
       if (!requestRef.current) {
         requestRef.current = new PresentationRequest(['/']);
       }
@@ -45,28 +29,29 @@ function usePresentation() {
     return terminateConnection;
   }, [connection, terminateConnection]);
 
-  // Listen for messages (from the controller to the presenter) and call handleMessage
-  useEffect(() => {
-    const handleConnectionList = list => {
-      list.connections.forEach(connection => {
-        connection.onmessage = ({ data }) => {
-          const event = JSON.parse(data);
-          // check if we have and handler for this event
-          const handlers = messageHandlers.current[event.type];
-          if (handlers) {
-            // if it exists then pass payload to slide
-            handlers.forEach(handler => handler(event.payload));
-          }
-        };
-      });
-    };
-    const receiver =
-      navigator && navigator.presentation && navigator.presentation.receiver;
+  // Add a message handler
+  const addMessageHandler = useCallback(handler => {    
+    const receiver = getReceiver();
     if (receiver) {
-      receiver.connectionList.then(handleConnectionList).catch(addError);
-      setIsReceiver(true);
+      const handleConnectionList = list => {
+        list.connections.forEach(connection => {
+          const oldHandler = connection.onmessage || (() => {});
+          connection.onmessage = event => {
+            const parsedData = JSON.parse(event.data)
+            handler(parsedData);
+            oldHandler(event)
+          };
+        });
+      };      
+      receiver.connectionList.then(handleConnectionList).catch(addError)
     }
-    return () => setIsReceiver(false);
+  }, []);
+
+  const terminateConnection = useCallback(() => {
+    if (connection) {
+      connection.terminate();
+      setConnection(null);
+    }
   }, [connection]);
 
   // Opens the display selection dialog box
@@ -75,26 +60,25 @@ function usePresentation() {
     if (request) {
       request
         .start()
-        .then(setConnection)
-        .catch(() =>
-          addError(new Error('User exited display selection dialog box'))
+        .then(connection => {
+          connection.onclose = () => setConnection(null); // Detect user closing presentation window
+          setConnection(connection);
+        })
+        .catch(e =>
+          addError(new Error('User (probably) exited display selection dialog box', e))
         );
     }
   }, []);
 
   // Send a message from the controller to the presenter
-  // type is the handler name e.g. slideDispatch0
   const sendMessage = useCallback(
-    (type, payload) => {
+    msg => {
       // This may throw if message isn't stringify-able
       try {
         if (connection) {
-          connection.send(
-            JSON.stringify({
-              type,
-              payload
-            })
-          );
+          connection.send(JSON.stringify(msg));
+        } else {
+          addError(new Error('Cannot send message before starting a conection'))
         }
       } catch (e) {
         addError(e);
@@ -108,9 +92,9 @@ function usePresentation() {
     terminateConnection,
     sendMessage,
     errors,
-    isReceiver,
     addMessageHandler,
-    hasConnection: !!connection
+    isReceiver: Boolean(getReceiver()),
+    isController: Boolean(connection)
   };
 }
 
