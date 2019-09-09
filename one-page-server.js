0a750dbd53293423219e20e68541c843327eb683
Ryan Roemer <ryan.roemer@formidable.com>
2019-09-08 21:31:05 -0700

    Tighten up server

--- one-page-server.js
+++ one-page-server.js
@@ -24,21 +24,15 @@ const resolve = async (url) => {
   // Already exists.
   const { pathname } = parse(url);
   const pathExists = await exists(path.join('.', pathname));
-  if (pathExists) {
-    return url;
-  }
+  if (pathExists) { return url; }
 
   // Use Node.js `require`-type resolution semantics if file doesn't end in `.js`.
   // A sub-implementation of https://nodejs.org/api/modules.html#modules_all_together
   const jsExists = await exists(path.join('.', `${pathname}.js`));
-  if (jsExists) {
-    return url.replace(pathname, `${pathname}.js`);
-  }
+  if (jsExists) { return url.replace(pathname, `${pathname}.js`); }
 
   const jsIndexExists = await exists(path.join('.', pathname, 'index.js'));
-  if (jsIndexExists) {
-    return url.replace(pathname, `${pathname}/index.js`);
-  }
+  if (jsIndexExists) { return url.replace(pathname, `${pathname}/index.js`); }
 
   // Base case: pass through.
   return url;
