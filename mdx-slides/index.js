60d6bc7aea6c09b505e5b593e63aa2cc8c54c27a
Jon Reynolds <jon.reynolds456@gmail.com>
2019-09-10 15:06:27 +0100

    initial component mapping in mdx

--- mdx-slides/index.js
+++ mdx-slides/index.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { MDXProvider } from '@mdx-js/react';
-import Deck from '../src/components/deck';
-import Slide from '../src/components/slide';
 
 // See the webpack config to see how this import alias is made
 import slides, { notes } from 'spectacle-user-mdx';
-import Notes from '../src/components/notes';
+import {
+  Deck,
+  Slide,
+  CodePane,
+  Image,
+  OrderedList,
+  Quote,
+  Heading,
+  UnorderedList,
+  Text,
+  ListItem,
+  Notes
+} from '../src/components';
 
-const components = {};
+const LeftAlignedHeading = props => <Heading {...props} textAlign="left" />;
+
+const components = {
+  p: Text,
+  // TODO - different heading sizes
+  h1: LeftAlignedHeading,
+  h2: LeftAlignedHeading,
+  h3: LeftAlignedHeading,
+  blockquote: Quote,
+  ul: UnorderedList,
+  ol: OrderedList,
+  li: ListItem,
+  // TODO - broken
+  img: Image,
+  codeblock: CodePane,
+  // pre: CodePane
+  code: CodePane
+};
 
 const MDXSlides = () => (
   <Deck loop>
