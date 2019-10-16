93ad6b0235e1ba1f05001d323020f770ca3a5375
Alex Saunders <alex.saunders@formidable.com>
2019-10-16 16:38:21 +0100

    plugin comments

--- config/plugins/mdx-plugin/index.js
+++ config/plugins/mdx-plugin/index.js
@@ -5,12 +5,14 @@ import parse from './parse';
 export default function mdx() {
   return {
     name: 'mdx',
-
     transform(code, id) {
       const ext = extname(id);
       if (ext !== '.mdx') {
+        // this is not an mdx file - move onto
+        // next plugin
         return code;
       }
+      // parse + return the mdx file content
       const parsedCode = parse(code);
       return parsedCode;
     }
