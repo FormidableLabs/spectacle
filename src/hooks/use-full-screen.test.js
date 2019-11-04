abd3ec5b58211f07e73a048c73754bc0de515e3e
kiraarghy <karaestubbs@gmail.com>
2019-11-04 18:09:59 +0000

    task/testing
    1. Fix eslint problems

--- src/hooks/use-full-screen.test.js
+++ src/hooks/use-full-screen.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, { shallow, mount } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import { useToggleFullScreen } from './use-full-screen';
