3d6ad424c864b8dd1de6a55a26fe65b5ebc5ed47
Carlos Kelly <carlos.paelinck@formidable.com>
2019-11-11 10:13:42 -0800

    Update Presenter Mode Design with Spectacle Logo (#770)
    * Update Presenter Mode Design with Spectacle Logo
    
    * Remove unused tag

--- src/components/deck/timer.js
+++ src/components/deck/timer.js
@@ -1,58 +1,27 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
-
 import { useTimer } from '../../utils/use-timer';
-import { Text } from '../typography';
-import { compose, color, typography } from 'styled-system';
-
-const Button = styled.button(
-  compose(
-    color,
-    typography
-  ),
-  css`
-    width: 4em;
-    height: 2em;
-    margin-right: 1em;
-    border: none;
-  `
-);
-
-const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin-left: 16px;
-`;
+import { Text, FlexBox, Box } from '../../';
+import InternalButton from '../internal-button';
 
 export const Timer = () => {
   const [timer, setTimer] = React.useState(0);
   const [timerStarted, setTimerStarted] = React.useState(false);
-  const addToTimer = React.useCallback(v => setTimer(s => s + v));
+  const addToTimer = React.useCallback(v => setTimer(s => s + v), []);
   useTimer(addToTimer, 1000, timerStarted);
   const minutes = Math.floor(timer.toFixed(0) / 60);
   return (
-    <>
-      <Text>{`${String(minutes).padStart(2, '0')}:${String(
-        timer.toFixed(0) - minutes * 60
-      ).padStart(2, '0')}`}</Text>
-      <ButtonContainer>
-        <Button onClick={() => setTimerStarted(s => !s)}>
-          {timerStarted ? 'Stop' : 'Start'}
-        </Button>
-        <Button
-          color="secondary"
-          backgroundColor="primary"
-          onClick={() => setTimer(0)}
-        >
-          Reset
-        </Button>
-      </ButtonContainer>
-    </>
+    <FlexBox>
+      <FlexBox justifyContent="flex-start" flex={1}>
+        <Text fontWeight="bold" fontSize={24}>{`${String(minutes).padStart(
+          2,
+          '0'
+        )}:${String(timer.toFixed(0) - minutes * 60).padStart(2, '0')}`}</Text>
+      </FlexBox>
+      <InternalButton onClick={() => setTimerStarted(s => !s)}>
+        {timerStarted ? 'Stop Timer' : 'Start Timer'}
+      </InternalButton>
+      <Box width={8} />
+      <InternalButton onClick={() => setTimer(0)}>Reset</InternalButton>
+    </FlexBox>
   );
-};
-
-Button.defaultProps = {
-  backgroundColor: 'secondary',
-  color: 'primary',
-  fontSize: '18px'
 };
