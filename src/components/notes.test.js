32e11fd4e071c96897bda2eabe60ea20d104414d
kiraarghy <karaestubbs@gmail.com>
2019-11-12 17:38:00 +0000

    fix notes case

--- src/components/notes.test.js
+++ src/components/notes.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import Notes from './Notes';
+import Notes from './notes';
 
 Enzyme.configure({ adapter: new Adapter() });
 
