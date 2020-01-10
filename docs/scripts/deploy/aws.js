6b2edcd9884136dc672e975f82812c48ff22f60f
Ryan Roemer <ryan.roemer@formidable.com>
2020-01-10 09:52:57 -0800

    Infra (CI/CD): Do dry run (fake) deployment to testing subdirectory in formidable.com bucket. (#791)
    - Fix the routes build logic to properly produce `docs` sidebar.
    - Add docs checks + build to CI.
    - Does a `--dryrun` simulated run on merging to a temporary `open-source/spectacle-TODO-TESTING` subdirectory in real production s3 bucket.

---
+++ docs/scripts/deploy/aws.js
@@ -0,0 +1,58 @@
+/**
+ * Upload docs to appropriate s3 subdirectory.
+ */
+const path = require('path');
+const chalk = require('chalk');
+const execa = require('execa');
+
+const PROJECT = 'spectacle';
+// TODO(docs): Convert to real location when ready for full release.
+const DOCS_PATH = `open-source/${PROJECT}-TODO-TESTING`;
+
+const SRC = path.resolve(__dirname, '../../dist');
+const BUCKET_NAME = 'formidable.com';
+const DEST = `s3://${path.join(BUCKET_NAME, DOCS_PATH)}`;
+
+const AWS_DRY_RUN_FLAG = '--dryrun';
+const AWS_EXCLUDES = ['*.DS_Store*'];
+
+// Cache values (in seconds)
+const CACHE_MAX_AGE_DEFAULT = 10 * 60; // eslint-disable-line no-magic-numbers
+
+const EXECA_OPTS = {
+  stdio: 'inherit'
+};
+
+const { log } = console;
+const logMsg = msg => log(chalk`[{cyan deploy/surge}] ${msg}`);
+
+const main = async ({ isDryRun }) => {
+  logMsg(`Uploading files to {cyan ${DEST}}`);
+  await execa(
+    'aws',
+    [
+      's3',
+      'sync',
+      isDryRun ? AWS_DRY_RUN_FLAG : '',
+      '--cache-control',
+      `max-age=${CACHE_MAX_AGE_DEFAULT},public`,
+      '--delete',
+      ...AWS_EXCLUDES.reduce(
+        (memo, exc) => memo.concat(['--exclude', exc]),
+        []
+      ),
+      SRC,
+      DEST
+    ].filter(Boolean),
+    EXECA_OPTS
+  );
+};
+
+if (require.main === module) {
+  main({
+    isDryRun: process.argv.indexOf('--dryrun') > -1
+  }).catch(err => {
+    console.error(err); // eslint-disable-line no-console
+    process.exit(1); // eslint-disable-line no-process-exit
+  });
+}
