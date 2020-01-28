a23979bc8397a43b6e67eab5c060862b6be6543b
Carlos Paelinck <me@carlos.dev>
2020-01-28 14:22:37 -0600

    Base types

---
+++ index.d.ts
@@ -0,0 +1,52 @@
+// Type definitions for Spectacle 6.0.0
+// Project: Formidable Spectacle
+// Definitions by: Kylie Stewart and Carlos Kelly
+
+declare module 'spectacle' {
+  import * as React from 'react';
+  import * as StyledSystem from 'styled-system';
+
+  export type TemplateFn = (options: {
+    numberOfSlides: number;
+    currentSlide: number;
+  }) => React.ReactNode;
+
+  export const Deck: React.FC<{
+    children: React.ReactNode;
+    animationsWhileGoingBack?: boolean;
+    backgroundColor?: string;
+    keyboardControls?: 'arrows' | 'space';
+    loop?: boolean;
+    theme?: Record<string, any>;
+    textColor?: string;
+    template?: TemplateFn;
+  }>;
+
+  export const Slide: React.FC<{
+    backgroundColor?: string;
+    children: React.ReactNode;
+    scaleRatio?: number;
+    textColor?: string;
+    template?: TemplateFn;
+  }>;
+
+  export const Appear: React.FC<{
+    children: React.ReactNode;
+    elementNum: number;
+    transitionEffect: {
+      from: Record<string, number | string>;
+      from: Record<string, number | string>;
+    };
+  }>;
+
+  type TypographyProps = {
+    children: React.ReactNode;
+  } & StyledSystem.ColorProps &
+    StyledSystem.TypographyProps &
+    StyledSystem.SpaceProps;
+
+  export const Text: React.FC<TypographyProps>;
+  export const CodeSpan: React.FC<TypographyProps>;
+  export const Heading: React.FC<TypographyProps>;
+  export const Link: React.FC<TypographyProps & React.AnchorHTMLAttributes>;
+}
