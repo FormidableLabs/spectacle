1fbde6cc996a71606bbf51bf4f1d56eb0f8ad715
Ryan Roemer <ryan.roemer@formidable.com>
2020-01-31 15:58:53 -0800

    Generates working presentation.

--- scripts/one-page.js
+++ scripts/one-page.js
@@ -2,10 +2,18 @@ codegen`
   const fs = require('fs');
   let code = fs.readFileSync(require.resolve('../examples/js/index.js'), 'utf8');
 
+  const extraImports = \`
+= Spectacle;
+
+import htm from 'https://unpkg.com/htm@^3?module';
+const html = htm.bind(React.createElement);
+  \`
+
   code = code
     .replace(/import React(|DOM) from 'react(|-dom)';/g, '')
-    // .replace(/import {$/, 'const {')
-    // .replace(/from 'spectacle'/, '= Spectacle')
+    .replace(/import {\\s*\\n*\\s*Appear/m, 'const { Appear')
+    .replace("from 'spectacle';", extraImports)
+    .replace('// SPECTACLE', '// SPECTACLE')
 
   module.exports = code;
 `;
