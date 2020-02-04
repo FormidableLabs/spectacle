4b30e7dfae3f72e9141ef0ebbdaaffd9a85f5c4f
Ryan Roemer <ryan.roemer@formidable.com>
2020-02-04 10:14:08 -0800

    Start on genned output mutations

--- scripts/one-page.js
+++ scripts/one-page.js
@@ -11,13 +11,27 @@ const { transformFileAsync } = require("@babel/core");
 const SRC_FILE = path.resolve(__dirname, '../examples/js/index.js');
 
 const main = async () => {
-  const { code } = await transformFileAsync(SRC_FILE, {
+  let { code } = await transformFileAsync(SRC_FILE, {
     babelrc: false,
     configFile: false,
     plugins: ['babel-plugin-transform-jsx-to-htm']
   });
 
-  console.log("TODO HERE", code);
+  // Mutate exports and comments.
+  code = code
+    // Mutate exports to our global imports.
+    .replace(/import React(|DOM) from 'react(|-dom)';[\n]*/gm, "")
+    .replace(/from 'spectacle';/, 'from Spectacle;')
+    // Hackily fix / undo babel's poor control comment placment.
+    .replace(/\/\/ SPECTACLE_CLI/gm, '\n// SPECTACLE_CLI')
+    .replace(/(\/\/ SPECTACLE_CLI[^\n]*)[\n]{2}/gm, '$1\n')
+    .replace(/(\/\/ SPECTACLE_CLI[^\n]*_START)/gm, '\n$1')
+
+  // Beautify htm snippets.
+  // TODO
+
+  // Output.
+  process.stdout.write(code);
 };
 
 if (require.main === module) {
