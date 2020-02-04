a417bd70ef3b83a6fcd9fbcb3dcb4de79a73bd92
Ryan Roemer <ryan.roemer@formidable.com>
2020-02-04 10:51:50 -0800

    Get auto-genning working.

--- scripts/one-page.js
+++ scripts/one-page.js
@@ -6,9 +6,27 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-const { transformFileAsync } = require("@babel/core");
+const { transformFileAsync } = require('@babel/core');
+const pretty = require('pretty');
 
 const SRC_FILE = path.resolve(__dirname, '../examples/js/index.js');
+
+const htmImport = `
+import htm from 'https://unpkg.com/htm@^3?module';
+const html = htm.bind(React.createElement);
+`
+  .replace(/  /gm, '')
+  .trim();
+
+const spectacleImportReplacer = (match, imports) => {
+  // Prettify imports
+  imports = imports
+    .split(',')
+    .map(i => `  ${i.trim()}`)
+    .join(`,\n`);
+
+  return `const {\n${imports}\n} = Spectacle;\n\n${htmImport}`;
+};
 
 const main = async () => {
   let { code } = await transformFileAsync(SRC_FILE, {
@@ -20,18 +38,35 @@ const main = async () => {
   // Mutate exports and comments.
   code = code
     // Mutate exports to our global imports.
-    .replace(/import React(|DOM) from 'react(|-dom)';[\n]*/gm, "")
-    .replace(/from 'spectacle';/, 'from Spectacle;')
+    .replace(/import React(|DOM) from 'react(|-dom)';[\n]*/gm, '')
+    .replace(/import {[ ]*(.*)} from 'spectacle';/, spectacleImportReplacer)
     // Hackily fix / undo babel's poor control comment placment.
     .replace(/\/\/ SPECTACLE_CLI/gm, '\n// SPECTACLE_CLI')
     .replace(/(\/\/ SPECTACLE_CLI[^\n]*)[\n]{2}/gm, '$1\n')
-    .replace(/(\/\/ SPECTACLE_CLI[^\n]*_START)/gm, '\n$1')
+    .replace(/(\/\/ SPECTACLE_CLI[^\n]*_START)/gm, '\n$1');
 
   // Beautify htm snippets.
-  // TODO
+  code = code.replace(
+    /(html`)(<\${[\s\S]*?}>)(`;)/gm,
+    (match, open, htm, close) => {
+      // Initial cleanup for inline strings and functions
+      htm = htm.replace(/>\${/gm, '>\n${').replace(/}<\/\${/gm, '}\n</${');
 
-  // Output.
-  process.stdout.write(code);
+      const formatted = `${open}${pretty(htm)}${close}`
+        // Initial newline
+        .replace('html`<${', 'html`\n<${')
+        // Indent
+        .split('\n')
+        .join('\n  ')
+        // Final newline
+        .replace('}>`;', '}>\n`;');
+
+      return formatted;
+    }
+  );
+
+  // TODO: Place in one-page.html
+  console.log(code);
 };
 
 if (require.main === module) {
