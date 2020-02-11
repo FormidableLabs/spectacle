384e86249e63bdbc74664e5b97504264c44eb424
Carlos Paelinck <me@carlos.dev>
2020-02-11 14:11:58 -0600

    Add support for Firefox

--- src/hooks/use-full-screen.js
+++ src/hooks/use-full-screen.js
@@ -5,6 +5,12 @@ export const useToggleFullScreen = () =>
     if (!document.fullscreenElement) {
       if ('requestFullScreen' in document.documentElement) {
         document.documentElement.requestFullscreen();
+      } else if ('mozRequestFullScreen' in document.documentElement) {
+        if (document.mozFullScreen) {
+          document.mozCancelFullScreen();
+          return;
+        }
+        document.documentElement.mozRequestFullScreen();
       } else if ('webkitRequestFullscreen' in document.documentElement) {
         if (document.webkitIsFullScreen) {
           document.webkitCancelFullScreen();
