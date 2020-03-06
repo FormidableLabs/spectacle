afdb9dcbabc076d586c9625c560dd9cc626b491c
Ryan Roemer <ryan.roemer@formidable.com>
2020-03-06 11:47:19 -0800

    various cleanup

--- docs/static-config-parts/static-webpack-config.js
+++ docs/static-config-parts/static-webpack-config.js
@@ -2,7 +2,22 @@
 // THAT ENHANCES THE BASE REACT-STATIC WEBPACK CONFIG.
 
 const staticWebpackConfig = (config, { defaultLoaders }) => {
+  // BUG/HACK: Disable CSS minification to avoid module build error:
+  // "Module build failed: BrowserslistError: Unknown browser query `dead`"
+  // https://github.com/FormidableLabs/spectacle/issues/857
+  defaultLoaders.cssLoader.loader.forEach(({ loader, options }) => {
+    if (loader === 'css-loader') {
+      options.minimize = false;
+    }
+  });
+
+  // Add development file info.
+  config.output.pathinfo =
+    config.output.pathinfo || process.env.REACT_STATIC_ENV === 'development';
+  // No source maps.
   config.devtool = false;
+
+  // Enhance rules.
   config.module.rules = [
     {
       oneOf: [
