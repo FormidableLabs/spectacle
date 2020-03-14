4b51e5b8575970349434f9864562c8fc1bb2ed42
Randal Vance Cunanan <randal@randalvance.net>
2020-03-14 13:22:51 +0800

    Fix Nested Appears

--- src/utils/search-children-appear.js
+++ src/utils/search-children-appear.js
@@ -2,14 +2,17 @@ import isComponentType from '../utils/is-component-type';
 import Appear from '../components/appear';
 
 export default function searchChildrenForAppear(children) {
-  if (!Array.isArray(children)) {
+  const isSingleChild = children?.props?.children ?? false;
+
+  if (!Array.isArray(children) && !isSingleChild) {
     return 0;
   }
-  return children.reduce((memo, current) => {
+
+  return (isSingleChild ? [children] : children).reduce((memo, current) => {
     if (isComponentType(current, Appear.name)) {
       memo += 1;
-    } else if (current?.props?.children?.length > 0) {
-      memo += searchChildrenForAppear(current.props.children);
+    } else {
+      memo += searchChildrenForAppear(current?.props?.children);
     }
     return memo;
   }, 0);
