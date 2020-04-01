87f62ca44e6c616b2f43a969c29875ab8f051e93
Ryan Roemer <ryan.roemer@formidable.com>
2020-04-01 13:26:32 -0700

    WIP: Start SSR script.

---
+++ scripts/ssr.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * Generate a fully server-rendered HTML page.
+ */
+process.env.BABEL_ENV = 'cjs';
+
+const path = require('path');
+const requireFromString = require('require-from-string');
+
+const { transformFileAsync } = require('@babel/core');
+
+const SRC_FILE = path.resolve(__dirname, '../examples/js/index.js');
+const SPECTACLE_CJS_PATH = path.resolve(__dirname, '../lib');
+
+const getSrcContent = async src => {
+  // Transform to Node-friendly form.
+  let { code } = await transformFileAsync(src, {
+    plugins: []
+  });
+
+  // Manually hack up some stuff.
+  code = code
+    // Switch to transpiled CJS
+    .replace('require("spectacle");', `require("${SPECTACLE_CJS_PATH}")`)
+    // Replace ReactDOM with an export.
+    .replace(
+      /^.*reactDom\.default\.render\(.*$/im,
+      'module.exports = Presentation;'
+    );
+
+  return code;
+};
+
+const main = async () => {
+  const code = await getSrcContent(SRC_FILE);
+  console.log('TODO HERE', code);
+  const Presentation = requireFromString(code);
+  console.log('TODO HERE', code);
+};
+
+if (require.main === module) {
+  main().catch(err => {
+    console.error(err); // eslint-disable-line no-console
+    process.exit(1); // eslint-disable-line no-process-exit
+  });
+}
