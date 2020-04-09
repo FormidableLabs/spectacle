c498d79940b95bcbc3d98ef2a41f326e87154bbf
Kylie Stewart <kylie@kyliestewart.tech>
2020-04-09 15:48:26 -0600

    svg cleanup - delete unused, add new

---
+++ docs/src/static/svgs/chevron.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SvgChevron = props => (
+  <svg viewBox="0 0 12 10" {...props}>
+    <path
+      d="M1.41 1L6 5.33 10.59 1 12 2.34 6 8 0 2.34z"
+      fill="currentColor"
+      fillRule="nonzero"
+    />
+  </svg>
+);
+
+export default SvgChevron;
