81e141a146dbcb7e14708cf1b2588d711436db5e
Carlos Kelly <me@carlos.dev>
2020-12-29 11:20:30 -0600

    Update usePresentation hook tests.

--- src/hooks/use-presentation.test.js
+++ src/hooks/use-presentation.test.js
@@ -1,61 +1,101 @@
-import React from 'react';
-import Enzyme, { mount } from 'enzyme';
+import 'regenerator-runtime/runtime';
+import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
+import { renderHook, act } from '@testing-library/react-hooks';
 import usePresentation from './use-presentation';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('usePresentation', () => {
-  it('when starting connection it calls start method on PresentationRequest and connection is set to true', () => {
-    const startMock = jest.fn().mockResolvedValue({});
-    class PresentationRequest {
-      constructor() {}
-      start = startMock;
-    }
-    global.PresentationRequest = PresentationRequest;
-    const TestComponent = () => {
-      const { startConnection } = usePresentation();
-      return (
-        <button
-          data-testid="startConnection"
-          onClick={() => startConnection()}
-        ></button>
-      );
-    };
-    const component = mount(<TestComponent />);
-    component.find('[data-testid="startConnection"]').simulate('click');
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.clearAllTimers();
+  });
+
+  it('when starting connection it calls start method on PresentationRequest.', async () => {
+    const startMock = jest.fn().mockResolvedValue({ terminate: () => {} });
+    global.PresentationRequest = jest
+      .fn()
+      .mockImplementationOnce(() => ({ start: startMock }));
+    const {
+      result: { current },
+      waitForNextUpdate
+    } = renderHook(() => usePresentation(), {});
+    await act(async () => {
+      current.startConnection();
+      await waitForNextUpdate();
+    });
     expect(startMock).toBeCalled();
   });
-  it('when calling terminate connection it calls startConnection', () => {
-    const terminateCallback = jest.fn();
+
+  it('assigns the connection object on start.', async () => {
+    const startMock = jest.fn().mockResolvedValue({ terminate: () => {} });
+    global.PresentationRequest = jest
+      .fn()
+      .mockImplementationOnce(() => ({ start: startMock }));
+    const { result, waitForValueToChange } = renderHook(
+      () => usePresentation(),
+      {}
+    );
+    await act(async () => {
+      result.current.startConnection();
+      await waitForValueToChange(() => {
+        jest.runAllTimers();
+        return result.current.connection;
+      });
+    });
+    expect(result.current.connection).not.toBeNull();
+  });
+
+  it('should send a stringified message to connection send handler.', async () => {
+    const sendMessageFn = jest.fn();
     const startMock = jest
       .fn()
-      .mockResolvedValue({ terminate: () => terminateCallback() });
-    class PresentationRequest {
-      constructor() {}
-      start = startMock;
-    }
-    global.PresentationRequest = PresentationRequest;
-    const TestComponent = () => {
-      const { terminateConnection, startConnection } = usePresentation();
-      return (
-        <>
-          <button
-            data-testid="startConnection"
-            onClick={() => startConnection()}
-          ></button>
-          <button
-            data-testid="terminate connection"
-            onClick={() => terminateConnection()}
-          ></button>
-        </>
-      );
-    };
-    const component = mount(<TestComponent />);
-    component.find('[data-testid="startConnection"]').simulate('click');
-    component.find('[data-testid="terminate connection"]').simulate('click');
-    // Having trouble testing this.
-    // expect(terminateCallback).toBeCalled();
+      .mockResolvedValue({ terminate: () => {}, send: sendMessageFn });
+    global.PresentationRequest = jest
+      .fn()
+      .mockImplementationOnce(() => ({ start: startMock }));
+    const { result, waitForValueToChange } = renderHook(
+      () => usePresentation(),
+      {}
+    );
+    await act(async () => {
+      result.current.startConnection();
+      await waitForValueToChange(() => {
+        jest.runAllTimers();
+        return result.current.connection;
+      });
+      result.current.sendMessage('Spectacle Message :)');
+    });
+    expect(sendMessageFn).toBeCalledWith(
+      JSON.stringify('Spectacle Message :)')
+    );
+  });
+
+  it('should close the connection on terminate.', async () => {
+    const terminateFn = jest.fn();
+    const startMock = jest
+      .fn()
+      .mockResolvedValue({ terminate: terminateFn, send: () => {} });
+    global.PresentationRequest = jest
+      .fn()
+      .mockImplementationOnce(() => ({ start: startMock }));
+    const { result, waitForValueToChange } = renderHook(
+      () => usePresentation(),
+      {}
+    );
+    await act(async () => {
+      result.current.startConnection();
+      await waitForValueToChange(() => {
+        jest.runAllTimers();
+        return result.current.connection;
+      });
+      result.current.terminateConnection();
+    });
+    expect(terminateFn).toBeCalled();
   });
 });
