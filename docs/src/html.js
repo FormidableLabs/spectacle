4ddf42aeb272315e8a1d17287b6a23873aee22ad
Carlos Kelly <me@carlos.dev>
2021-01-12 07:04:02 -0600

    Cherry Pick: 971 from main

--- docs/src/html.js
+++ docs/src/html.js
@@ -27,14 +27,34 @@ const CustomDocument = ({ Html, Head, Body, children }) => {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="./favicon/Favicon32.png"
+          href="/favicon/favicon-32x32.png"
         />
-        <link rel="manifest" href="./site.webmanifest" />
-        <meta name="msapplication-TileColor" content="#ff4081" />
-        <meta name="msapplication-config" content="./browserconfig.xml" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta
+          name="msapplication-config"
+          content="/favicon/browserconfig.xml"
+        />
         <meta name="theme-color" content="#ffffff" />
         <link
           href="https://fonts.googleapis.com/css?family=Space+Mono&display=swap"
