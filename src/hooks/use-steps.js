07db9a20c427a49669bfa8b6c3c1b3292c24c81a
Sasha Schwartz <soft_circles@Sashas-MacBook-Pro.local>
2021-04-21 13:45:10 -0500

    fix prettier error

--- src/hooks/use-steps.js
+++ src/hooks/use-steps.js
@@ -11,7 +11,7 @@ const PLACEHOLDER_CLASS_NAME = 'step-placeholder';
  * Hook to determine whether or not a step is active or not. Note that
  * useCollectSteps() must be called prior to calling this method as useSteps()
  * requires activationThresholds to be available.
-*/
+ */
 export function useSteps(numSteps = 1, { id: userProvidedId, stepIndex } = {}) {
   const [stepId] = React.useState(userProvidedId || ulid);
 
