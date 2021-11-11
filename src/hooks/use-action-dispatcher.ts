d5852abb8d717a83d8f3eae98e137f466db4d3bf
scottrippey <scott.william.rippey@gmail.com>
2021-11-11 16:07:53 -0700

    feature(typescript): convert hook: `use-action-dispatcher`

--- src/hooks/use-action-dispatcher.ts
+++ src/hooks/use-action-dispatcher.ts
@@ -8,13 +8,18 @@ function identity(sender) {
  * Wrapper for react dispatch function. The third argument is an optional
  * function to identify the dispatcher i.e. { slide: 1, step: 2 }
  */
-export default function useActionDispatcher(
-  dispatch,
-  type,
-  payloadCreator = identity
+export default function useActionDispatcher<
+  TDispatch extends (action: { type: TType; payload: TPayload }) => void,
+  TType extends string,
+  TArgs extends unknown[],
+  TPayload
+>(
+  dispatch: TDispatch,
+  type: TType,
+  payloadCreator: (...args: TArgs) => TPayload = identity as any
 ) {
   const dispatcher = React.useCallback(
-    (...args) => {
+    (...args: TArgs) => {
       dispatch({ type, payload: payloadCreator(...args) });
     },
     [dispatch, type, payloadCreator]
