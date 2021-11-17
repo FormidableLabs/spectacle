9b11608ff89c6326103ce66704ccddfc765a2332
Scott Rippey <scottrippey@users.noreply.github.com>
2021-11-17 14:22:24 -0700

    Chore: prune obsolete hooks (#1074)
    * chore: removed unused, obsolete hooks
    
    * chore: removed unused, obsolete utils
    
    * chore: removed unused, obsolete dependencies
    
    Co-authored-by: scottrippey <scott.william.rippey@gmail.com>

--- src/utils/detect-platform.js
+++
@@ -1,7 +0,0 @@
-export const isWindows = () =>
-  navigator.platform.toLowerCase().includes('win32');
-
-export const isMacOS = () =>
-  navigator.platform.toLowerCase().includes('macintel');
-
-export const isLinux = () => navigator.platform.toLowerCase().includes('linux');
