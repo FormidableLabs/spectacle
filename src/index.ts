94dd47f271e5c912e4916ddbb4b9ff7e6b1a22fc
scottrippey <scott.william.rippey@gmail.com>
2021-11-17 13:38:49 -0700

    feature(typescript): improved default exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import Deck from './components/deck';
-import Slide, { SlideContext } from './components/slide/slide';
-import { Appear, Stepper } from './components/appear';
-import CodePane from './components/code-pane';
-import {
+export { default as Deck } from './components/deck';
+export { default as Slide, SlideContext } from './components/slide/slide';
+export { Appear, Stepper } from './components/appear';
+export { default as CodePane } from './components/code-pane';
+export {
   OrderedList,
   Quote,
   Heading,
@@ -12,73 +12,29 @@ import {
   Link,
   CodeSpan
 } from './components/typography';
-import {
+export {
   Table,
   TableCell,
   TableRow,
   TableHeader,
   TableBody
 } from './components/table';
-import { FlexBox, Grid, Box } from './components/layout';
-import { Image, FullSizeImage } from './components/image';
-import Notes from './components/notes';
-import Progress from './components/progress';
-import FullScreen from './components/fullscreen';
-import {
+export { FlexBox, Grid, Box } from './components/layout';
+export { Image, FullSizeImage } from './components/image';
+export { default as Notes } from './components/notes';
+export { default as Progress } from './components/progress';
+export { default as FullScreen } from './components/fullscreen';
+export {
   Markdown,
   MarkdownSlideSet,
   MarkdownSlide,
   MarkdownPreHelper
 } from './components/markdown/markdown';
-import SpectacleLogo from './components/logo';
-import mdxComponentMap from './utils/mdx-component-mapper';
-import { removeNotes, isolateNotes } from './utils/notes';
-import indentNormalizer from './utils/indent-normalizer';
-import { DeckContext } from './components/deck/deck';
-import useMousetrap from './hooks/use-mousetrap';
-import defaultTheme from './theme/default-theme';
-import { fadeTransition, slideTransition } from './components/transitions';
-
-export {
-  Appear,
-  Deck,
-  Slide,
-  CodePane,
-  Box,
-  FlexBox,
-  Grid,
-  Image,
-  FullSizeImage,
-  OrderedList,
-  Quote,
-  Heading,
-  ListItem,
-  UnorderedList,
-  Text,
-  Link,
-  CodeSpan,
-  Notes,
-  Progress,
-  FullScreen,
-  Markdown,
-  MarkdownSlideSet,
-  MarkdownSlide,
-  MarkdownPreHelper,
-  SpectacleLogo,
-  Stepper,
-  Table,
-  TableCell,
-  TableRow,
-  TableHeader,
-  TableBody,
-  mdxComponentMap,
-  DeckContext,
-  SlideContext,
-  removeNotes,
-  isolateNotes,
-  indentNormalizer,
-  defaultTheme,
-  useMousetrap,
-  fadeTransition,
-  slideTransition
-};
+export { default as SpectacleLogo } from './components/logo';
+export { default as mdxComponentMap } from './utils/mdx-component-mapper';
+export { removeNotes, isolateNotes } from './utils/notes';
+export { default as indentNormalizer } from './utils/indent-normalizer';
+export { DeckContext } from './components/deck/deck';
+export { default as useMousetrap } from './hooks/use-mousetrap';
+export { default as defaultTheme } from './theme/default-theme';
+export { fadeTransition, slideTransition } from './components/transitions';
