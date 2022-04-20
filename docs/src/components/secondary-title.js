9b62697654e33c4fe23bfc4639e1dfee415bef04
JP <jpdriver@users.noreply.github.com>
2022-04-20 13:51:30 -0700

    replace /docs dir

--- docs/src/components/secondary-title.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const SecondaryTitle = styled.h3`
-  color: ${({ theme }) => theme.colors.textDark};
-  font-size: 2rem;
-  line-height: 2.4rem;
-  text-align: center;
-`;
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -22,6 +22,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          path: '../docs',
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl: 'https://github.com/FormidableLabs/spectacle/tree/main/website',
           breadcrumbs: false,
