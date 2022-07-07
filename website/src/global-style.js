ba4349432cbc77cf88f3e128556b74d555d2ee69
Ryan Srofe <ryan.srofe@formidable.com>
2022-07-07 14:58:31 -0400

    various global styles

--- website/src/global-style.js
+++ website/src/global-style.js
@@ -9,16 +9,16 @@ export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    background: ${(p) => p.theme.colors.textLight};
-    color: ${(p) => p.theme.colors.text};
-    font-family: ${(p) => p.theme.fonts.body};
-    font-size: ${(p) => p.theme.fontSizes.bodySmall};
-    font-weight: ${(p) => p.theme.fontWeights.body};  
-    line-height: ${(p) => p.theme.lineHeights.body};
+    background: ${({ theme }) => theme.colors.bgLight};
+    color: ${({ theme }) => theme.colors.textDark};
+    font-family: ${({ theme }) => theme.fonts.body};
+    font-size: ${({ theme }) => theme.fontSizes.bodySmall};
+    font-weight: ${({ theme }) => theme.fontWeights.body};  
+    line-height: ${({ theme }) => theme.lineHeights.body};
     text-rendering: optimizeLegibility;
 
-    @media ${(p) => p.theme.media.lg} {
-      font-size: ${(p) => p.theme.fontSizes.body};
+    @media ${({ theme }) => theme.media.lg} {
+      font-size: ${({ theme }) => theme.fontSizes.body};
     }
   }
 
@@ -30,35 +30,33 @@ export const GlobalStyle = createGlobalStyle`
 
   a {
     text-decoration: none;
-    color: ${(p) => p.theme.colors.accentLight};
-    font-weight: ${(p) => p.theme.fontWeights.links};
+    color: ${({ theme }) => theme.colors.link};
+    font-weight: ${({ theme }) => theme.fontWeights.links};
 
     &:hover {
-      color: ${(p) => p.theme.colors.button};
-      background: ${(p) => p.theme.colors.button};
+      color: ${({ theme }) => theme.colors.linkHover};
       text-decoration: none;
     }
   }
 
   table, pre, p, h1, h2, h3 {
-    margin: 0 0 ${(p) => p.theme.spacing.md} 0;
+    margin: 0 0 ${({ theme }) => theme.spacing.md} 0;
   }
 
   h1, h2, h3 {
-    font-family: ${(p) => p.theme.fonts.heading};
-    font-weight: ${(p) => p.theme.fontWeights.heading};
-    line-height: ${(p) => p.theme.lineHeights.heading};
-    color: ${(p) => p.theme.colors.heading};
+    font-weight: ${({ theme }) => theme.fontWeights.heading};
+    line-height: ${({ theme }) => theme.lineHeights.heading};
+    color: ${({ theme }) => theme.colors.textLight};
   }
 
   h1 {
-    font-size: ${(p) => p.theme.fontSizes.h1};
+    font-size: ${({ theme }) => theme.fontSizes.h1};
   }
   h2 {
-    font-size: ${(p) => p.theme.fontSizes.h2};
+    font-size: ${({ theme }) => theme.fontSizes.h2};
   }
   h3 {
-    font-size: ${(p) => p.theme.fontSizes.h3};
+    font-size: ${({ theme }) => theme.fontSizes.h3};
   }
   img {
     max-width: 100%;
