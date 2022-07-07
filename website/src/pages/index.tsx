388580b82211f45b033d6809e63af351e6cfa4c8
Ryan Srofe <ryan.srofe@formidable.com>
2022-07-07 15:00:04 -0400

    import paths, added content as props, and theme provider

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,30 +1,29 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import { GlobalStyle } from '@site/src/global-style';
 import { theme } from '@site/src/theme';
 import Header from '@site/src/components/global/header';
 import Footer from '@site/src/components/global/footer';
-import Features from '@site/src/components/home/features';
-import Preview from '@site/src/components/home/preview';
-import GetStarted from '@site/src/components/home/get-started';
-import MoreOSS from '@site/src/components/home/more-oss';
-import content from '@site/src/components/home/_content';
+import Features from '@site/src/components/index/features';
+import Preview from '@site/src/components/index/preview';
+import GetStarted from '@site/src/components/index/get-started';
+import MoreOSS from '@site/src/components/index/more-oss';
+import content from '@site/src/components/index/_content';
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
+  const { header, footer, features, preview, getStarted, oss } = content;
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Header />
+      <Header content={header} />
       <main>
-        <Features />
-        <Preview preview={content.preview} />
-        <GetStarted getStarted={content.getStarted} />
-        <MoreOSS oss={content.oss} />
+        <Features content={features} />
+        <Preview content={preview} />
+        <GetStarted content={getStarted} />
+        <MoreOSS content={oss} />
       </main>
-      <Footer />
+      <Footer content={footer} />
     </ThemeProvider>
   );
 }
