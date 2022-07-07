2491777d6c57f45b5d8292399818de4b1993b3f7
Ryan Srofe <ryan.srofe@formidable.com>
2022-07-07 14:56:34 -0400

    various updates to config for docusaurus
    updates for sass, alpha, there are some comments in here that are still in testing for nav items and footer

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -4,6 +4,47 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+function romanize(num) {
+  if (!+num) return false;
+  const digits = String(+num).split('');
+  const key = [
+    '',
+    'C',
+    'CC',
+    'CCC',
+    'CD',
+    'D',
+    'DC',
+    'DCC',
+    'DCCC',
+    'CM',
+    '',
+    'X',
+    'XX',
+    'XXX',
+    'XL',
+    'L',
+    'LX',
+    'LXX',
+    'LXXX',
+    'XC',
+    '',
+    'I',
+    'II',
+    'III',
+    'IV',
+    'V',
+    'VI',
+    'VII',
+    'VIII',
+    'IX'
+  ];
+  let roman = '';
+  let i = 3;
+  while (i--) roman = (key[+digits.pop() + i * 10] || '') + roman;
+  return Array(+digits.join('') + 1).join('M') + roman;
+}
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Spectacle',
@@ -23,42 +64,54 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          path: '../docs',
-          sidebarPath: require.resolve('./sidebars.js'),
+          breadcrumbs: false,
           editUrl:
             'https://github.com/FormidableLabs/spectacle/tree/main/website',
-          breadcrumbs: false
+          path: '../docs',
+          sidebarCollapsible: true,
+          sidebarCollapsed: true,
+          sidebarPath: require.resolve('./sidebars.js'),
+          showLastUpdateAuthor: false,
+          showLastUpdateTime: false
         },
         googleAnalytics: {
-          trackingID: 'UA-43290258-1',
-          anonymizeIP: true
+          anonymizeIP: true,
+          trackingID: 'UA-43290258-1'
         },
         gtag: {
-          trackingID: 'GTM-MD32945',
-          anonymizeIP: true
+          anonymizeIP: true,
+          trackingID: 'GTM-MD32945'
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css')
+          customCss: require.resolve('./src/css/custom.scss')
         }
       })
     ]
   ],
 
+  plugins: ['docusaurus-plugin-sass'],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Spectacle',
+        title: 'SPECTACLE',
         logo: {
           alt: 'Spectacle Logo',
-          src: 'img/logo.svg'
+          src: 'svg/nav-logo.svg'
         },
         items: [
           // {
           //   type: 'doc',
-          //   docId: 'basic-concepts',
+          //   docId: 'advanced-concepts',
           //   position: 'left',
-          //   label: 'Documentation'
+          //   label: 'Testing Internal Link'
+          // },
+          // {
+          //   href: 'https://github.com/FormidableLabs/spectacle',
+          //   'aria-label': 'GitHub Repository',
+          //   label: 'Testing External Link',
+          //   position: 'left'
           // },
           {
             href: 'https://github.com/FormidableLabs/spectacle',
@@ -75,8 +128,45 @@ const config = {
         ]
       },
       footer: {
-        style: 'dark',
-        copyright: `Copyright © ${new Date().getFullYear()} Formidable Labs, LLC.`
+        logo: {
+          alt: 'Formidable Labs Logo',
+          src: 'svg/formidable-icon-white.svg'
+        },
+        copyright: `© ${romanize(
+          new Date().getFullYear()
+        )} Formidable Labs, LLC.`
+        // links: [
+        //   {
+        //     items: [
+        //       {
+        //         html: `<img src="./src/img/logo_formidable_white.png" alt="Formidable Labs Logo" />`
+        //       }
+        //     ]
+        //   },
+        //   {
+        //     items: [
+        //       {
+        //         label: 'Open Source', //Link text
+        //         href: '#' //Link target
+        //       },
+        //       {
+        //         label: 'Contact',
+        //         href: '#'
+        //       },
+        //       {
+        //         label: 'Careers',
+        //         href: '#'
+        //       }
+        //     ]
+        //   },
+        //   {
+        //     items: [
+        //       {
+        //         html: `<p>Formidable is a global design and engineering consultancy and open source software organization, specializing in React.js, React Native, GraphQL, Node.js, and the extended JavaScript ecosystem. We have locations in Seattle, London, Toronto, Denver, and Phoenix with remote consultants worldwide. For more information please visit formidable.com.</p>`
+        //       }
+        //     ]
+        //   }
+        // ]
       },
       prism: {
         theme: lightCodeTheme,
