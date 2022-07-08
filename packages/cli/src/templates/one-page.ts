6a4705367e425f38a23a51a44dcee6941e786b9f
Carlos Kelly <me@carlos.dev>
2022-07-08 14:31:59 -0500

    Add Base CLI package (#1142)
    * Added base CLI package
    
    * Added nodemon config file for cross-platform support.
    
    * Fix nodemon
    
    * Clean up gitignore, outpath for directory.

---
+++ packages/cli/src/templates/one-page.ts
@@ -0,0 +1,48 @@
+type OnePageTemplateOptions = {
+  name: string;
+};
+
+export const onePageTemplate = ({ name }: OnePageTemplateOptions) => `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>${name}</title>
+  </head>
+  <body>
+    <div id="root"></div>
+
+    <script src="https://unpkg.com/react@18.1.0/umd/react.production.min.js"></script>
+    <script src="https://unpkg.com/react-dom@18.1.0/umd/react-dom.production.min.js"></script>
+    <script src="https://unpkg.com/react-is@18.1.0/umd/react-is.production.min.js"></script>
+    <script src="https://unpkg.com/prop-types@15.7.2/prop-types.min.js"></script>
+    <script src="https://unpkg.com/spectacle@^9/dist/spectacle.min.js"></script>
+
+    <script type="module">
+      const {
+        FlexBox,
+        SpectacleLogo,
+        Slide,
+        Deck,
+      } = Spectacle;
+
+      import htm from 'https://unpkg.com/htm@^3?module';
+      const html = htm.bind(React.createElement);
+
+      const Presentation = () => html\`
+        <\${Deck}>
+          <\${Slide}>
+            <\${FlexBox} height="100%">
+              <\${SpectacleLogo} size=${500} />
+            </\${FlexBox}>
+          </\${Slide}>
+        </\${Deck}>
+      \`;
+      const root = ReactDOM.createRoot(document.getElementById('root'));
+      root.render(html\`<\${Presentation}/>\`);
+    </script>
+  </body>
+</html>
+`;
