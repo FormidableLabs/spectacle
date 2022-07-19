b42c2daf9cfe98626eb4d3dbd8e10022db702767
Grant Sander <gksander93@gmail.com>
2022-07-19 14:02:28 -0500

    Redrirect plugin

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -86,7 +86,20 @@ const config = {
     ]
   ],
 
-  plugins: ['docusaurus-plugin-sass'],
+  plugins: [
+    'docusaurus-plugin-sass',
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [
+          {
+            from: '/docs/basic-concepts/',
+            to: '/docs/'
+          }
+        ]
+      }
+    ]
+  ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
