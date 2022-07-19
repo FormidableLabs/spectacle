83a47f643116146156157f192e5b9ee9f6e27d33
Ryan Srofe <ryan.srofe@formidable.com>
2022-07-19 11:31:38 -0400

    remove testing link

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -106,12 +106,6 @@ const config = {
           },
           {
             href: 'https://github.com/FormidableLabs/spectacle',
-            'aria-label': 'GitHub Repository',
-            position: 'left',
-            label: 'External Link'
-          },
-          {
-            href: 'https://github.com/FormidableLabs/spectacle',
             className: 'header-github-link',
             'aria-label': 'GitHub Repository',
             position: 'right',
