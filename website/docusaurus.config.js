ea4edb0c3ed336ba573954a8ccf736b612c93a0c
Grant Sander <gksander93@gmail.com>
2022-08-08 12:31:52 -0500

    Small tweak to docusaurus config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -42,116 +42,117 @@ function romanize(num) {
   return Array(+digits.join('') + 1).join('M') + roman;
 }
 
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-  title: 'Spectacle',
-  tagline:
-    'A React.js based library for creating sleek presentations using JSX syntax that gives you the ability to live demo your code.',
-  url: 'https://formidable.com',
-  baseUrl: '/open-source/spectacle/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'formidablelabs',
-  projectName: 'spectacle',
+async function createConfig() {
+  /** @type {import("@docusaurus/types").Config} */
+  const config = {
+    title: 'Spectacle',
+    tagline:
+      'A React.js based library for creating sleek presentations using JSX syntax that gives you the ability to live demo your code.',
+    url: 'https://formidable.com',
+    baseUrl: '/open-source/spectacle/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'formidablelabs',
+    projectName: 'spectacle',
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
+    presets: [
+      [
+        'classic',
+        /** @type {import("@docusaurus/preset-classic").Options} */
+        ({
+          docs: {
+            breadcrumbs: false,
+            editUrl:
+              'https://github.com/FormidableLabs/spectacle/tree/main/website',
+            path: '../docs',
+            sidebarCollapsible: true,
+            sidebarCollapsed: true,
+            sidebarPath: require.resolve('./sidebars.js'),
+            showLastUpdateAuthor: false,
+            showLastUpdateTime: false
+          },
+          googleAnalytics: {
+            anonymizeIP: true,
+            trackingID: 'UA-43290258-1'
+          },
+          gtag: {
+            anonymizeIP: true,
+            trackingID: 'GTM-MD32945'
+          },
+          theme: {
+            customCss: [require.resolve('./src/css/custom.scss')]
+          }
+        })
+      ]
+    ],
+
+    plugins: [
+      'docusaurus-plugin-sass',
+      [
+        '@docusaurus/plugin-client-redirects',
+        {
+          redirects: [
+            {
+              from: '/docs/basic-concepts/',
+              to: '/docs/'
+            }
+          ]
+        }
+      ]
+    ],
+
+    themeConfig:
+    /** @type {import("@docusaurus/preset-classic").ThemeConfig} */
       ({
-        docs: {
-          breadcrumbs: false,
-          editUrl:
-            'https://github.com/FormidableLabs/spectacle/tree/main/website',
-          path: '../docs',
-          sidebarCollapsible: true,
-          sidebarCollapsed: true,
-          sidebarPath: require.resolve('./sidebars.js'),
-          showLastUpdateAuthor: false,
-          showLastUpdateTime: false
+        navbar: {
+          title: 'SPECTACLE',
+          logo: {
+            alt: 'Spectacle Logo',
+            src: 'svg/nav-logo.svg'
+          },
+          items: [
+            {
+              type: 'doc',
+              docId: 'index',
+              position: 'left',
+              label: 'Docs'
+            },
+            {
+              href: 'https://github.com/FormidableLabs/spectacle',
+              className: 'header-github-link',
+              'aria-label': 'GitHub Repository',
+              position: 'right',
+              label: 'GitHub Repository'
+            },
+            {
+              href: 'https://formidable.com',
+              className: 'header-formidable-link',
+              'aria-label': 'Formidable Website',
+              position: 'right',
+              label: 'Formidable Website'
+            }
+          ]
         },
-        googleAnalytics: {
-          anonymizeIP: true,
-          trackingID: 'UA-43290258-1'
+        footer: {
+          logo: {
+            alt: 'Formidable Labs Logo',
+            src: 'svg/formidable-logo-white.svg',
+            href: 'https://formidable.com',
+            target: '_blank'
+          },
+          copyright: `© ${romanize(
+            new Date().getFullYear()
+          )} Formidable Labs, LLC.`
         },
-        gtag: {
-          anonymizeIP: true,
-          trackingID: 'GTM-MD32945'
-        },
-        theme: {
-          customCss: [require.resolve('./src/css/custom.scss')]
+        prism: {
+          // theme: lightCodeTheme,
+          // darkTheme: darkCodeTheme
         }
       })
-    ]
-  ],
+  };
 
-  plugins: [
-    'docusaurus-plugin-sass',
-    [
-      '@docusaurus/plugin-client-redirects',
-      {
-        redirects: [
-          {
-            from: '/docs/basic-concepts/',
-            to: '/docs/'
-          }
-        ]
-      }
-    ]
-  ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'SPECTACLE',
-        logo: {
-          alt: 'Spectacle Logo',
-          src: 'svg/nav-logo.svg'
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'index',
-            position: 'left',
-            label: 'Docs'
-          },
-          {
-            href: 'https://github.com/FormidableLabs/spectacle',
-            className: 'header-github-link',
-            'aria-label': 'GitHub Repository',
-            position: 'right',
-            label: 'GitHub Repository'
-          },
-          {
-            href: 'https://formidable.com',
-            className: 'header-formidable-link',
-            'aria-label': 'Formidable Website',
-            position: 'right',
-            label: 'Formidable Website'
-          }
-        ]
-      },
-      footer: {
-        logo: {
-          alt: 'Formidable Labs Logo',
-          src: 'svg/formidable-logo-white.svg',
-          href: 'https://formidable.com',
-          target: '_blank'
-        },
-        copyright: `© ${romanize(
-          new Date().getFullYear()
-        )} Formidable Labs, LLC.`
-      },
-      prism: {
-        // theme: lightCodeTheme,
-        // darkTheme: darkCodeTheme
-      }
-    })
-};
-
-async function createConfig() {
   const lightTheme = (await import('./src/utils/prismLight.mjs')).default;
   const darkTheme = (await import('./src/utils/prismDark.mjs')).default;
   // @ts-expect-error: we know it exists, right
--- website/src/utils/prismLight.mjs
+++ website/src/utils/prismLight.mjs
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import lightTheme from 'prism-react-renderer/themes/github/index.cjs.js';
 
 export default {
