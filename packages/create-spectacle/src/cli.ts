cc0cf72e1e2e3f62090b3f367c21881f3a598948
Grant Sander <gksander93@gmail.com>
2022-08-10 15:57:45 -0500

    Overwrite flags

--- packages/create-spectacle/src/cli.ts
+++ packages/create-spectacle/src/cli.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+import fs from 'node:fs';
+import path from 'node:path';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import chalk from 'chalk';
@@ -15,19 +17,22 @@ import {
 import { version, devDependencies } from '../package.json';
 
 const argv = yargs(hideBin(process.argv)).argv;
+const cwd = process.cwd();
+
 enum ArgName {
   type = 'type',
   name = 'name',
   lang = 'lang',
-  port = 'port'
+  port = 'port',
+  overwrite = 'overwrite'
 }
 
-type CLIOptions = {
-  [ArgName.type]: 'tsx' | 'jsx' | 'mdx' | 'onepage';
-  [ArgName.name]: string;
-  [ArgName.lang]?: string;
-  [ArgName.port]?: number;
-};
+const DeckTypeOptions = [
+  { title: chalk.cyan('tsx'), value: 'tsx' },
+  { title: chalk.yellow('jsx'), value: 'jsx' },
+  { title: chalk.red('mdx'), value: 'mdx' },
+  { title: chalk.green('One Page'), value: 'onepage' }
+];
 
 let progressInterval: NodeJS.Timer;
 const log = console.log;
@@ -40,57 +45,93 @@ const main = async () => {
 
   let i = 0;
   let type = argv[ArgName.type] || argv['t'];
-  let name = argv[ArgName.name] || argv['n'];
+  let name = argv['_']?.[0];
   let lang = argv[ArgName.lang] || argv['l'] || 'en';
   let port = argv[ArgName.port] || argv['p'] || 3000;
 
+  const isTryingToOverwrite = Boolean(name) && !isFolderNameAvailable(name);
+
   /**
    * If type/name not both provided via CLI flags, prompt for them.
-   * TODO: Handle abort signal. If user aborts during prompt, we should respect that.
    */
-  if (!(type && name)) {
-    const response = await prompts([
-      !type && {
-        type: 'select',
-        name: ArgName.type as string,
-        message: 'What type of deck do you want to create?',
-        choices: [
-          { title: 'tsx', value: 'tsx' },
-          { title: 'jsx', value: 'jsx' },
-          { title: 'mdx', value: 'mdx' },
-          { title: 'One Page', value: 'onepage' }
-        ]
-      },
-      !name && {
-        type: 'text',
-        name: ArgName.name as string,
-        message: 'What is the name of the presentation?',
-        // TODO: Validate path doesn't already exist
-        validate: async (val) => {
-          return val.length > 0 ? true : 'Name is required';
+  if (!(type && name && lang && port) || isTryingToOverwrite) {
+    try {
+      const response = await prompts(
+        [
+          // Name prompt
+          {
+            type: 'text',
+            name: ArgName.name as string,
+            message: 'What is the name of the presentation?',
+            initial: name,
+            validate: async (val) => {
+              return val.length > 0 ? true : 'Name is required';
+            }
+          },
+          // If output directory already exists, prompt to overwrite
+          {
+            type: (val) => (isFolderNameAvailable(val) ? null : 'confirm'),
+            name: ArgName.overwrite as string,
+            message: (val) =>
+              `Target directory ${formatProjectDirName(
+                val
+              )} already exists. Overwrite and continue?`
+          },
+          // Check overwrite comes back false, we need to abort.
+          {
+            type: (_, answers) => {
+              if (answers?.[ArgName.overwrite] === false) {
+                throw new Error('❌ Operation cancelled');
+              }
+              return null;
+            },
+            name: 'overwriteAborter'
+          },
+          {
+            type: 'select',
+            name: ArgName.type as string,
+            message: 'What type of deck do you want to create?',
+            choices: DeckTypeOptions,
+            initial: (() => {
+              const ind = DeckTypeOptions.findIndex((o) => o.value === type);
+              return ind > -1 ? ind : 0;
+            })()
+          },
+          // Language prompt
+          {
+            type: 'text',
+            name: ArgName.lang as string,
+            message:
+              'What is the language code for the generated HTML document?',
+            initial: lang,
+            validate: async (val) => {
+              return val.length > 0 ? true : 'Language code is required';
+            }
+          },
+          {
+            // Don't prompt for this if onepage
+            type: (_, answers) =>
+              answers?.[ArgName.type] === 'onepage' ? null : 'text',
+            name: ArgName.port as string,
+            message: 'What port should the webpack dev server run on?',
+            initial: port
+          }
+        ],
+        {
+          onCancel: () => {
+            throw new Error('❌ Operation cancelled');
+          }
         }
-      },
-      {
-        type: 'text',
-        name: ArgName.lang as string,
-        message: 'What is the language code for the generated HTML document?',
-        initial: lang,
-        validate: (val) => {
-          return val.length > 0 ? true : 'Language code is required';
-        }
-      },
-      {
-        type: 'number',
-        name: ArgName.port as string,
-        message: 'What port should the webpack dev server run on?',
-        initial: port
-      }
-    ]);
+      );
 
-    if (response.type) type = response.type;
-    if (response.name) name = response.name;
-    lang = response.lang;
-    port = response.port;
+      if (response.type) type = response.type;
+      if (response.name) name = response.name;
+      lang = response.lang;
+      port = response.port;
+    } catch (err) {
+      console.log(chalk.red(err.message));
+      return;
+    }
   }
 
   progressInterval = setInterval(() => {
@@ -104,7 +145,7 @@ const main = async () => {
   await sleep(750);
 
   const fileOptions: FileOptions = {
-    snakeCaseName: name.toLowerCase().replace(/([^a-z0-9]+)/gi, '-'),
+    snakeCaseName: formatProjectDirName(name),
     name,
     lang,
     port,
@@ -129,6 +170,14 @@ const main = async () => {
     chalk.whiteBright.bgGreen.bold(` \u2714 `),
     chalk.green.bold(`A new ${type} deck named ${name} was created.\n`)
   );
+};
+
+const formatProjectDirName = (name: string) =>
+  name.toLowerCase().replace(/([^a-z0-9]+)/gi, '-');
+
+const isFolderNameAvailable = (name: string) => {
+  const dir = path.join(cwd, formatProjectDirName(name));
+  return !fs.existsSync(dir);
 };
 
 main().catch((err) => {
