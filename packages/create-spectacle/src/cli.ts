4579af5de1ff14702f41982bb6405dee091f4a1f
Grant Sander <gksander93@gmail.com>
2022-08-10 11:47:24 -0500

    Use workspace dependency

--- packages/create-spectacle/src/cli.ts
+++ packages/create-spectacle/src/cli.ts
@@ -9,7 +9,7 @@ import {
   writeWebpackProjectFiles,
   writeOnePageHTMLFile
 } from './templates/file-writers';
-import { version as SPECTACLE_VERSION } from 'spectacle/package.json';
+import { version as SPECTACLE_VERSION } from '../package.json';
 
 type CLIOptions = {
   type: 'tsx' | 'jsx' | 'mdx' | 'onepage';
