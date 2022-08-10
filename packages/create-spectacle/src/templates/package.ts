0d9d35680f05e391761b6d7270d63080b528bd9e
Ryan Roemer <ryan.roemer@formidable.com>
2022-08-10 11:30:47 -0700

    Handle workspace in CI tests

--- packages/create-spectacle/src/templates/package.ts
+++ packages/create-spectacle/src/templates/package.ts
@@ -14,7 +14,11 @@ export const packageTemplate = (options: PackageTemplateOptions) =>
     "build": "webpack --config ./webpack.config.js --mode production"
   },
   "dependencies": {
-    "spectacle": "^${options.spectacleVersion}",
+    "spectacle": "${
+      options.spectacleVersion === 'workspace:*'
+        ? '*'
+        : `^${options.spectacleVersion}`
+    }",
     "react": "^18.1.0",
     "react-dom": "^18.1.0"
   },
