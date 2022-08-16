0f2055e43f01084cda3bc9461d023bed53bdb9d2
Christine Zimmerman <christineortega@gmail.com>
2022-08-16 16:43:29 -0700

    Add slide layout options: Section, Statement, Big fact, Quote.

--- packages/spectacle/src/components/slide-layout.tsx
+++ packages/spectacle/src/components/slide-layout.tsx
@@ -1,7 +1,7 @@
 import Slide, { SlideProps } from './slide/slide';
 import { Box, FlexBox } from './layout-primitives';
 import { ComponentProps, Fragment, ReactNode } from 'react';
-import { Heading, ListItem, OrderedList, UnorderedList } from './typography';
+import { Heading, Text, ListItem, OrderedList, UnorderedList } from './typography';
 import { Appear } from './appear';
 
 /**
@@ -83,13 +83,59 @@ const List = ({
 };
 
 /**
+ * Section layout with left aligned text
+ */
+ const Section = ({ sectionTitle, sectionTitleProps, ...rest }: Omit<SlideProps, 'children'> & { sectionTitle: string; sectionTitleProps?: ComponentProps<typeof Heading>; }) => (
+  <Slide {...rest}>
+    <FlexBox justifyContent="flex-start" height="100%">
+      <Heading {...sectionTitleProps}>{sectionTitle}</Heading>
+    </FlexBox>
+  </Slide>
+);
+
+/**
+ * Statement layout with centered text
+ */
+ const Statement = ({ statement, statementProps, ...rest }: Omit<SlideProps, 'children'> & { statement: string; statementProps?: ComponentProps<typeof Heading>;}) => (
+  <Slide {...rest}>
+    <FlexBox height="100%">
+      <Heading {...statementProps}>{statement}</Heading>
+    </FlexBox>
+  </Slide>
+);
+
+/**
+ * Big Fact with optional fact information
+ */
+const BigFact = ({ fact, factInformation, factProps, factInformationProps, ...rest }: Omit<SlideProps, 'children'> & {fact: string; factInformation?: string; factProps?: ComponentProps<typeof Text>; factInformationProps?: ComponentProps<typeof Text>;}) => (
+  <Slide {...rest}>
+    <FlexBox>
+      <Box>
+        <Text fontSize="250px" textAlign="center" {...factProps}>{fact}</Text>
+          {factInformation ? 
+          <Text textAlign="center" {...factInformationProps}>{factInformation}</Text>
+          : null}
+      </Box>
+    </FlexBox>
+  </Slide>
+);
+
+/**
+ * Quote layout
+ */
+ const Quote = ({ quote, quoteProps, attribution, attributionProps, ...rest }: Omit<SlideProps, 'children'> & {quote: string; quoteProps?: ComponentProps<typeof Text>; attribution: string; attributionProps?: ComponentProps<typeof Text>}) => (
+  <Slide {...rest}>
+    <Box width="100%" margin="auto">
+        <Text fontSize="85px" {...quoteProps}>{quote}</Text>
+        <Text fontSize="36px" padding={'0em 0em 0em 1em'} {...attributionProps}>{attribution}</Text>
+    </Box>
+  </Slide>
+);
+/**
  * Layouts to consider:
  * - Image (left, right, full bleed?)
  * - Intro
- * - Quote
- * - Section
- * - Statement?
- * - Big fact?
+ * - Code Snippet (syntax highlighting)
  */
 
-export default { Full, Center, TwoColumn, List };
+export default { Full, Center, TwoColumn, List, Section, BigFact, Quote, Statement  };
