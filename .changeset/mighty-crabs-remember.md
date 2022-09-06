7e69146597350b347f954ac2d6ed0589916e3421
Grant Sander <gksander93@gmail.com>
2022-09-06 10:47:34 -0400

    `create-spectacle`: vite starters, add simple-ish test regarding file generation (#1210)
    * Initial vite support
    
    * Use JSON.stringify
    
    * Initial test setup
    
    * Explicate outDir
    
    * Update colors
    
    * Add comment
    
    * Add changeset
    
    * Remove unused variables
    
    * Remove whacky colors for prompt

---
+++ .changeset/mighty-crabs-remember.md
@@ -0,0 +1,5 @@
+---
+'create-spectacle': minor
+---
+
+Add vite starter kits to create-spectacle. Add tests to test expected files were generated.
--- packages/create-spectacle/src/templates/readme.ts
+++ packages/create-spectacle/src/templates/readme.ts
@@ -1,11 +1,13 @@
 type ReadmeTemplateOptions = {
   name: string;
   enableTypeScriptSupport: boolean;
+  isVite?: boolean;
 };
 
 export const readmeTemplate = ({
   name,
-  enableTypeScriptSupport
+  enableTypeScriptSupport,
+  isVite
 }: ReadmeTemplateOptions) =>
   `
 # ${name}
@@ -24,5 +26,11 @@ Made with ❤️ and [Spectacle](https://github.com/FormidableLabs/spectacle/).
 
 To build your presentation for a production deploy, run \`yarn build\` (or \`npm build\` or \`pnpm build\`).
 
-The build artifacts will be placed in the \`dist\` directory. If you'd like to change this location, edit \`output.path\` in \`webpack.config.js\`.
+The build artifacts will be placed in the \`dist\` directory. If you'd like to change this location, edit ${
+    isVite
+      ? `\`build.outDir\` in \`vite.config.${
+          enableTypeScriptSupport ? 'ts' : 'js'
+        }\``
+      : `\`output.path\` in \`webpack.config.js\``
+  }.
 `.trim();
