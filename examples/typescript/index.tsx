f8196ab88d21d8265f30b813ffbdef3062c22769
Christine Zimmerman <christineortega@gmail.com>
2022-09-26 13:04:19 -0700

    Return example to normal.

--- examples/typescript/index.tsx
+++ examples/typescript/index.tsx
@@ -27,26 +27,6 @@ import { createRoot } from 'react-dom/client';
 const formidableLogo =
   'https://avatars2.githubusercontent.com/u/5078602?s=280&v=4';
 
-const formidableDogs = {
-  sophie:
-    'https://raw.githubusercontent.com/FormidableLabs/dogs/main/src/sophie.jpg',
-  lucy: 'https://raw.githubusercontent.com/FormidableLabs/dogs/main/src/lucy.jpg',
-  boba: 'https://raw.githubusercontent.com/FormidableLabs/dogs/main/src/boba.jpg',
-  otis: 'https://raw.githubusercontent.com/FormidableLabs/dogs/main/src/otis.jpg',
-  penelope:
-    'https://raw.githubusercontent.com/FormidableLabs/dogs/main/src/penelope.jpg',
-  odie: 'https://raw.githubusercontent.com/FormidableLabs/dogs/main/src/odie.jpg',
-  fred: 'https://raw.githubusercontent.com/FormidableLabs/dogs/main/src/fred.jpg',
-  islay:
-    'https://raw.githubusercontent.com/FormidableLabs/dogs/main/src/islay.jpg',
-  beau: 'https://raw.githubusercontent.com/FormidableLabs/dogs/main/src/beau.jpg',
-  rusty:
-    'https://raw.githubusercontent.com/FormidableLabs/dogs/main/src/rusty.jpg',
-  madden:
-    'https://raw.githubusercontent.com/FormidableLabs/dogs/main/src/madden.jpg',
-  neve: 'https://raw.githubusercontent.com/FormidableLabs/dogs/main/src/neve.jpg'
-};
-
 // SPECTACLE_CLI_THEME_START
 const theme = {
   fonts: {
@@ -91,151 +71,203 @@ const SlideFragments = () => (
   </>
 );
 
-const codeExample = `
-    <SlideLayout.MultiCodeLayout
-      numColumns={2}
-      title={'How to JavaScript'}
-      codeBlocks={[
-        {
-          code: 'let greeting = 'hello Spectacle.',
-          language: 'jsx',
-          description: 'assign a variable to a string',
-          descriptionProps: { color: 'quaternary' }
-        },
-        {
-          code: 'greeting = 'hello again Spectacle.',
-          language: 'jsx',
-          description: 'reassign the variable',
-          descriptionProps: { color: 'quaternary' }
-        }
-      ]}
-    />`;
-
 const Presentation = () => (
   <Deck theme={theme} template={template}>
-    <SlideLayout.Full>
-      <Heading>Welcome to Spectacle</Heading>
-      <Text textAlign="center">Full Layout provides an blank slate.</Text>
-    </SlideLayout.Full>
-    <SlideLayout.Center>
-      <Text>Center Layout centers content of any kind. </Text>
-    </SlideLayout.Center>
-    <SlideLayout.Section>Great for title pages!</SlideLayout.Section>
-    <SlideLayout.Statement>
-      Great for title pages and statements.
-    </SlideLayout.Statement>
-    <SlideLayout.TwoColumn
-      left={
-        <>
-          <Heading>Left</Heading>{' '}
-          <Text>{`Formatted columns make a big impact.`}</Text>
-          <Text>{`Let's try...`}</Text>
-        </>
-      }
-      right={
-        <>
-          <Heading>Right</Heading>
-          <Text fontWeight="bold">
-            {`I've learned that people will forget what you said, people will
-            forget what you did, but people will never forget how you made them
-            feel.`}
-          </Text>
-          <Text fontWeight="bold">{` -Maya Angelou`}</Text>
-        </>
-      }
-    />
-    <SlideLayout.BigFact factInformation={'We earned 100%!'}>
-      100%
-    </SlideLayout.BigFact>
-    <SlideLayout.Quote attribution={'William Shakespeare'}>
-      To be, or not to be...
-    </SlideLayout.Quote>
-    <SlideLayout.Quote
-      quoteProps={{ fontSize: '68px' }}
-      attribution={'Maya Angelou'}
-      attributionProps={{ fontSize: '48px' }}
-    >
-      {`I've learned that people will forget what you said, people will forget
-      what you did, but people will never forget how you made them feel.`}
-    </SlideLayout.Quote>
-    <SlideLayout.Code
-      language={'jsx'}
-      title={
-        'Here is an example of the SlideLayout code used to generate the next slide.'
-      }
-      titleProps={{ fontSize: '38px' }}
-    >
-      {codeExample}
-    </SlideLayout.Code>
-    <SlideLayout.MultiCodeLayout
-      numColumns={2}
-      title={'How to JavaScript'}
-      codeBlocks={[
-        {
-          code: `let greeting = 'hello Spectacle.'`,
-          language: `jsx`,
-          description: `assign a variable to a string`,
-          descriptionProps: { color: 'quaternary' }
+    <Slide>
+      <FlexBox height="100%">
+        <SpectacleLogo size={500} />
+      </FlexBox>
+      <Notes>
+        Spectacle supports notes per slide.
+        <ol>
+          <li>Notes can now be HTML markup!</li>
+          <li>Lists can make it easier to make points.</li>
+        </ol>
+      </Notes>
+    </Slide>
+    <Slide>
+      <FlexBox height="100%" flexDirection="column">
+        <Heading margin="0px" fontSize="150px">
+          ✨<i>Spectacle</i> ✨
+        </Heading>
+        <Heading margin="0px" fontSize="h2">
+          A ReactJS Presentation Library
+        </Heading>
+        <Heading margin="0px 32px" color="primary" fontSize="h3">
+          Where you can write your decks in JSX, Markdown, or MDX!
+        </Heading>
+      </FlexBox>
+    </Slide>
+    <Slide
+      transition={{
+        from: {
+          transform: 'scale(0.5) rotate(45deg)',
+          opacity: 0
         },
-        {
-          code: `greeting = 'hello again Spectacle.'`,
-          language: `jsx`,
-          description: `reassign the variable`,
-          descriptionProps: { color: 'quaternary' }
+        enter: {
+          transform: 'scale(1) rotate(0)',
+          opacity: 1
+        },
+        leave: {
+          transform: 'scale(0.2) rotate(315deg)',
+          opacity: 0
         }
+      }}
+      backgroundColor="tertiary"
+      backgroundImage="url(https://github.com/FormidableLabs/dogs/blob/main/src/beau.jpg?raw=true)"
+      backgroundOpacity={0.5}
+    >
+      <Heading>Custom Backgrounds</Heading>
+      <UnorderedList>
+        <ListItem>
+          <CodeSpan>backgroundColor</CodeSpan>
+        </ListItem>
+        <ListItem>
+          <CodeSpan>backgroundImage</CodeSpan>
+        </ListItem>
+        <ListItem>
+          <CodeSpan>backgroundOpacity</CodeSpan>
+        </ListItem>
+        <ListItem>
+          <CodeSpan>backgroundSize</CodeSpan>
+        </ListItem>
+        <ListItem>
+          <CodeSpan>backgroundPosition</CodeSpan>
+        </ListItem>
+        <ListItem>
+          <CodeSpan>backgroundRepeat</CodeSpan>
+        </ListItem>
+      </UnorderedList>
+    </Slide>
+    <Slide>
+      <Heading>Animated Elements</Heading>
+      <OrderedList>
+        <Appear>
+          <ListItem>Elements can animate in!</ListItem>
+        </Appear>
+        <Appear>
+          <ListItem>Out of order</ListItem>
+        </Appear>
+        <Appear priority={0}>
+          <ListItem>
+            Just identify the order with the prop <CodeSpan>priority</CodeSpan>!
+          </ListItem>
+        </Appear>
+      </OrderedList>
+    </Slide>
+    <Slide>
+      <FlexBox>
+        <Text>These</Text>
+        <Text>Text</Text>
+        <Text color="secondary">Items</Text>
+        <Text fontWeight="bold">Flex</Text>
+      </FlexBox>
+      <Grid gridTemplateColumns="1fr 2fr" gridColumnGap={15}>
+        <Box backgroundColor="primary">
+          <Text color="secondary">Single-size Grid Item</Text>
+        </Box>
+        <Box backgroundColor="secondary">
+          <Text>Double-size Grid Item</Text>
+        </Box>
+      </Grid>
+      <Grid
+        gridTemplateColumns="1fr 1fr 1fr"
+        gridTemplateRows="1fr 1fr 1fr"
+        gridRowGap={1}
+      >
+        {Array(9)
+          .fill('')
+          .map((_, index) => (
+            <FlexBox paddingTop={0} key={`formidable-logo-${index}`} flex={1}>
+              <Image src={formidableLogo} width={100} />
+            </FlexBox>
+          ))}
+      </Grid>
+    </Slide>
+    <SlideFragments />
+    <Slide>
+      <CodePane language="jsx">{`
+        import { createClient, Provider } from 'urql';
+        const client = createClient({ url: 'https://0ufyz.sse.codesandbox.io' });
+        const App = () => (
+          <Provider value={client}>
+            <Todos />
+          </Provider>
+        );
+        `}</CodePane>
+      <Box height={20} />
+      <CodePane language="java" showLineNumbers={false}>{`
+        public class NoLineNumbers {
+          public static void main(String[] args) {
+            System.out.println("Hello");
+          }
+        }
+        `}</CodePane>
+    </Slide>
+    <div>
+      <Slide>
+        <Heading>This is a slide embedded in a div</Heading>
+      </Slide>
+    </div>
+    <MarkdownSlide componentProps={{ color: 'yellow' }}>
+      {`
+        # This is a Markdown Slide
+        - You can pass props down to all elements on the slide.
+        - Just use the \`componentProps\` prop.
+        `}
+    </MarkdownSlide>
+    <MarkdownSlide animateListItems>
+      {`
+       # This is also a Markdown Slide
+       It uses the \`animateListItems\` prop.
+       - Its list items...
+       - ...will appear...
+       - ...one at a time.
+      `}
+    </MarkdownSlide>
+    <Slide>
+      <Grid
+        gridTemplateColumns="50% 50%"
+        gridTemplateRows="50% 50%"
+        height="100%"
+      >
+        <FlexBox alignItems="center" justifyContent="center">
+          <Heading>This is a 4x4 Grid</Heading>
+        </FlexBox>
+        <FlexBox alignItems="center" justifyContent="center">
+          <Text textAlign="center">
+            With all the content aligned and justified center.
+          </Text>
+        </FlexBox>
+        <FlexBox alignItems="center" justifyContent="center">
+          <Text textAlign="center">
+            It uses Spectacle <CodeSpan>{'<Grid />'}</CodeSpan> and{' '}
+            <CodeSpan>{'<FlexBox />'}</CodeSpan> components.
+          </Text>
+        </FlexBox>
+        <FlexBox alignItems="center" justifyContent="center">
+          <Box width={200} height={200} backgroundColor="secondary" />
+        </FlexBox>
+      </Grid>
+    </Slide>
+    <MarkdownSlideSet>
+      {`
+        # This is the first slide of a Markdown Slide Set
+        ---
+        # This is the second slide of a Markdown Slide Set
+        `}
+    </MarkdownSlideSet>
+    <SlideLayout.List
+      title="Slide layouts!"
+      items={[
+        'Title pages',
+        'Two-column',
+        'Lists',
+        'Code blocks',
+        'Images',
+        'And more!'
       ]}
-    />
-    <SlideLayout.HorizontalImage
-      src={formidableDogs.neve}
-      title={'Show me the dogs!'}
-      alt={'Neve the dog panting while laying in a garden bed.'}
-    />
-    <SlideLayout.HorizontalImage
-      src={formidableDogs.madden}
-      title={'Madden the dog is so cute'}
-      description={'We love him'}
-      alt={'Studio portrait of Madden the dog.'}
-    />
-    <SlideLayout.ThreeUpImage
-      primary={{
-        src: formidableDogs.beau,
-        alt: 'Beau the dog laying on the couch looking at the camera.',
-        position: 'left'
-      }}
-      top={{
-        src: formidableDogs.madden,
-        alt: 'Madden the dog with a seriously regal look on his face.'
-      }}
-      bottom={{
-        src: formidableDogs.neve,
-        alt: 'Neve the dog laying in a flower bed.'
-      }}
-    />
-    <SlideLayout.VerticalImage
-      src={formidableDogs.odie}
-      listItems={[
-        'Odie the dog',
-        'color of sand',
-        'super wet dog',
-        'super happy pup'
-      ]}
-      position={'right'}
-      alt={'Odie the dog'}
-    />
-    <SlideLayout.VerticalImage
-      src={formidableDogs.lucy}
-      listItems={['Good girl', 'Happy dog', 'Spotty', 'Silly']}
-      title={'Lucy'}
-      alt={'Lucy the dog'}
-    />
-    <SlideLayout.FullBleedImage
-      src={formidableDogs.fred}
-      imgContainerProps={{ style: { border: '8px white solid' } }}
-      alt={'Fred looking at the camera'}
-    />
-    <SlideLayout.FullBleedImage
-      src={formidableDogs.madden}
-      alt={'Madden the dog'}
+      animateListItems
     />
   </Deck>
 );
