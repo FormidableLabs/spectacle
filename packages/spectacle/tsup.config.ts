bfe221d2aaa0512a65ea41df3d11d262947275dd
Grant Sander <gksander93@gmail.com>
2023-05-09 11:56:44 -0500

    "UMD" build with tsup (#1268)
    * WIP
    
    * Regen lock
    
    * Remove dist scripts
    
    * Update spectacle umd script
    
    * Add back in process dep
    
    * Add changeset
    
    * minify umd output
    
    * Update local script

--- packages/spectacle/tsup.config.ts
+++ packages/spectacle/tsup.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'tsup';
+import externalGlobal from 'esbuild-plugin-external-global';
 
 export default defineConfig({
   entry: ['src/index.ts'],
@@ -7,6 +8,27 @@ export default defineConfig({
   clean: false,
   external: ['react', 'react-dom'],
   dts: true,
-  format: ['cjs', 'esm'],
-  target: 'es6'
+  format: ['cjs', 'esm', 'iife'],
+  skipNodeModulesBundle: true,
+  globalName: 'Spectacle',
+  target: 'es6',
+
+  /**
+   * Couple build options for UMD/iife build.
+   *  - externalGlobalPlugin to use window.React instead of trying to bundle it.
+   *  - for iife build, set platform to "browser" and define process.env.NODE_ENV
+   */
+  esbuildPlugins: [
+    externalGlobal.externalGlobalPlugin({
+      react: 'window.React',
+      'react-dom': 'window.ReactDOM'
+    })
+  ],
+  esbuildOptions: (options, context) => {
+    if (context.format === 'iife') {
+      options.minify = true;
+      options.platform = 'browser';
+      options.define!['process.env.NODE_ENV'] = '"production"';
+    }
+  }
 });
