7257c855772563eb21c561f40602e74bd88115d8
Carlos Kelly <me@carlos.dev>
2023-07-22 15:39:31 -0500

    Annotate the tests explaining each action.

--- packages/spectacle/src/hooks/use-deck-state.test.ts
+++ packages/spectacle/src/hooks/use-deck-state.test.ts
@@ -7,6 +7,10 @@ describe('useDeckState', () => {
     stepIndex: 1
   };
 
+  /**
+   * The INITIALIZE_TO should set the active and pending views
+   * to the values provided in the payload.
+   */
   it('should handle INITIALIZE_TO action', () => {
     const { result } = renderHook(() => useDeckState(initialState));
 
@@ -19,6 +23,11 @@ describe('useDeckState', () => {
     expect(result.current.initialized).toBe(true);
   });
 
+  /**
+   * The SKIP_TO action should set the pending view slide index to
+   * the slide index provided by the payload and set the navigation
+   * direction based on a delta of the previous and pending slides.
+   */
   it('should handle SKIP_TO action', () => {
     const { result } = renderHook(() => useDeckState(initialState));
 
@@ -26,9 +35,27 @@ describe('useDeckState', () => {
       result.current.skipTo({ slideIndex: 3 });
     });
 
+    expect(result.current.navigationDirection).toBe(1);
     expect(result.current.pendingView.slideIndex).toBe(3);
   });
 
+  it('should handle SKIP_TO action in reverse', () => {
+    const { result } = renderHook(() =>
+      useDeckState({ slideIndex: 5, stepIndex: 0, slideId: 0 })
+    );
+
+    act(() => {
+      result.current.skipTo({ slideIndex: 3 });
+    });
+
+    expect(result.current.navigationDirection).toBe(-1);
+    expect(result.current.pendingView.slideIndex).toBe(3);
+  });
+
+  /**
+   * The STEP_FORWARD action should increment the pending slide index by 1
+   * and have a positive navigation direction.
+   */
   it('should handle STEP_FORWARD action', () => {
     const { result } = renderHook(() => useDeckState(initialState));
 
@@ -42,6 +69,10 @@ describe('useDeckState', () => {
     expect(result.current.navigationDirection).toBe(1);
   });
 
+  /**
+   * The STEP_FORWARD action should decrement the pending slide index by 1
+   * and have a negative navigation direction.
+   */
   it('should handle STEP_BACKWARD action', () => {
     const { result } = renderHook(() => useDeckState(initialState));
 
@@ -55,6 +86,10 @@ describe('useDeckState', () => {
     expect(result.current.navigationDirection).toBe(-1);
   });
 
+  /**
+   * The ADVANCE_SLIDE action should increment the pending slide index by 1,
+   * reset the step index, and have a positive navigation direction.
+   */
   it('should handle ADVANCE_SLIDE action', () => {
     const { result } = renderHook(() => useDeckState(initialState));
 
@@ -69,6 +104,10 @@ describe('useDeckState', () => {
     expect(result.current.navigationDirection).toBe(1);
   });
 
+  /**
+   * The REGRESS_SLIDE action should decrement the pending slide index by 1,
+   * reset the step index, and have a negative navigation direction.
+   */
   it('should handle REGRESS_SLIDE action', () => {
     const { result } = renderHook(() => useDeckState(initialState));
 
@@ -83,6 +122,10 @@ describe('useDeckState', () => {
     expect(result.current.navigationDirection).toBe(-1);
   });
 
+  /**
+   * The COMMIT_TRANSITION action should set the active slide view
+   * and pending slide view to the payload values.
+   */
   it('should handle COMMIT_TRANSITION action', () => {
     const { result } = renderHook(() => useDeckState(initialState));
 
@@ -94,6 +137,10 @@ describe('useDeckState', () => {
     expect(result.current.pendingView).toEqual({ slideIndex: 2, stepIndex: 2 });
   });
 
+  /**
+   * The CANCEL_TRANSITION action should cancel the slide transition
+   * by reverting the pending view values to what the current active slide values are.
+   */
   it('should handle CANCEL_TRANSITION action', () => {
     const { result } = renderHook(() => useDeckState(initialState));
 
