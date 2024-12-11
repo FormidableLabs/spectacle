aec76b753b30bad6a5a01abaed7c4e9824ff9ba9
Ryan Roemer <ryan.roemer@formidable.com>
2024-12-10 17:23:59 -0800

    Doesn't work, but builds now.

--- packages/spectacle/src/components/typography.tsx
+++ packages/spectacle/src/components/typography.tsx
@@ -10,8 +10,8 @@ import {
   SpaceProps,
   BorderProps
 } from 'styled-system';
-import { FC, PropsWithChildren } from 'react';
-export { FitText } from './typography/fit-text';
+import { FC, PropsWithChildren, useRef, useState } from 'react';
+import useResizeObserver from 'use-resize-observer';
 
 const decoration = system({ textDecoration: true });
 type DecorationProps = Pick<CSSObject, 'textDecoration'>;
@@ -114,6 +114,52 @@ ListItem.defaultProps = {
   margin: 0
 };
 
+const FitContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ScalableText = styled(Text)<{ scale: number }>`
+  transform-origin: center left;
+  transform: scale(${(props) => props.scale});
+  white-space: nowrap;
+  max-width: ${(props) => `${100 / props.scale}%`};
+` as any; // TODO: FIX TS ERROR (remove `any`).
+ScalableText.defaultProps = {
+  ...Text.defaultProps,
+  scale: 1
+};
+
+const FitText: FC<CommonTypographyProps> = (props) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const textRef = useRef<HTMLDivElement>(null);
+  const [scale, setScale] = useState(1);
+
+  useResizeObserver({
+    ref: containerRef,
+    onResize: () => {
+      if (!containerRef.current || !textRef.current) return;
+
+      const containerWidth = containerRef.current.offsetWidth;
+      const textWidth = textRef.current.offsetWidth;
+
+      if (textWidth === 0) return;
+
+      const newScale = Math.min(containerWidth / textWidth, 1);
+      setScale(newScale);
+    }
+  });
+
+  return (
+    <FitContainer ref={containerRef}>
+      <ScalableText {...props} ref={textRef} scale={scale} />
+    </FitContainer>
+  );
+};
+
+
 export {
   Text,
   Heading,
@@ -122,5 +168,6 @@ export {
   UnorderedList,
   ListItem,
   Link,
-  CodeSpan
+  CodeSpan,
+  FitText
 };
